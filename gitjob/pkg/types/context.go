32335d503e4780b27691622a921a700a808a7509
Daishan <daishan@rancher.com>
2020-08-15 00:21:37 -0700

    Add helm chart support

--- gitjob/pkg/types/context.go
+++ gitjob/pkg/types/context.go
@@ -3,16 +3,13 @@ package types
 import (
 	"context"
 
-	v1 "github.com/rancher/gitjob/pkg/apis/gitjob.cattle.io/v1"
 	"github.com/rancher/gitjob/pkg/generated/controllers/gitjob.cattle.io"
 	"github.com/rancher/wrangler/pkg/apply"
 	"github.com/rancher/wrangler/pkg/crd"
 	"github.com/rancher/wrangler/pkg/generated/controllers/batch"
 	"github.com/rancher/wrangler/pkg/generated/controllers/core"
-	"github.com/rancher/wrangler/pkg/schemas/openapi"
 	"github.com/rancher/wrangler/pkg/start"
 	"github.com/sirupsen/logrus"
-	"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 )
@@ -64,26 +61,10 @@ func BuildContext(ctx context.Context, namespace string, config *rest.Config) (c
 		logrus.Fatal(err)
 	}
 
-	factory.BatchCreateCRDs(ctx, getCRDs()...)
-
 	if err := factory.BatchWait(); err != nil {
 		logrus.Fatal(err)
 	}
 
 	c := NewContext(namespace, config)
 	return context.WithValue(ctx, contextKey{}, c), c
-}
-
-func getCRDs() []crd.CRD {
-	return []crd.CRD{
-		crd.NamespacedType("GitJob.gitjob.cattle.io/v1").WithStatus().WithSchema(mustSchema(v1.GitJob{})),
-	}
-}
-
-func mustSchema(obj interface{}) *v1beta1.JSONSchemaProps {
-	result, err := openapi.ToOpenAPIFromStruct(obj)
-	if err != nil {
-		panic(err)
-	}
-	return result
 }
