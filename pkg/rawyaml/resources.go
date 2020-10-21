83bb5e78bc1ca3ca5f476b2462c2a7451608f92e
Darren Shepherd <darren@rancher.com>
2020-10-21 08:31:31 -0700

    Do not process raw yaml files as templates unless in templates/ folder

---
+++ pkg/rawyaml/resources.go
@@ -0,0 +1,36 @@
+package rawyaml
+
+import (
+	"bytes"
+	"strings"
+
+	"github.com/rancher/wrangler/pkg/yaml"
+	"helm.sh/helm/v3/pkg/chart"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+const (
+	YAMLPrefix    = "chart/raw-yaml/"
+	inChartPrefix = "raw-yaml/"
+)
+
+func ToObjects(c *chart.Chart) (result []runtime.Object, _ error) {
+	for _, resource := range c.Files {
+		if !strings.HasPrefix(resource.Name, inChartPrefix) {
+			continue
+		}
+		objs, err := yaml.ToObjects(bytes.NewBuffer(resource.Data))
+		if err != nil {
+			return nil, err
+		}
+		for _, obj := range objs {
+			apiVersion, kind := obj.GetObjectKind().GroupVersionKind().ToAPIVersionAndKind()
+			if apiVersion == "" || kind == "" {
+				continue
+			}
+			result = append(result, obj)
+		}
+	}
+
+	return result, nil
+}
