e8211b511b92d6673db1327b588dfce7ff3b208d
Daishan <daishan@rancher.com>
2021-04-23 21:23:42 -0700

    Use kustomize for fleet-agent

--- pkg/controllers/manageagent/manageagent.go
+++ pkg/controllers/manageagent/manageagent.go
@@ -2,6 +2,8 @@ package manageagent
 
 import (
 	"context"
+	"fmt"
+	"sort"
 
 	"github.com/rancher/fleet/pkg/agent"
 	fleet "github.com/rancher/fleet/pkg/apis/fleet.cattle.io/v1alpha1"
@@ -9,13 +11,13 @@ import (
 	fleetcontrollers "github.com/rancher/fleet/pkg/generated/controllers/fleet.cattle.io/v1alpha1"
 	"github.com/rancher/wrangler/pkg/apply"
 	corecontrollers "github.com/rancher/wrangler/pkg/generated/controllers/core/v1"
-	"github.com/rancher/wrangler/pkg/name"
 	"github.com/rancher/wrangler/pkg/relatedresource"
 	"github.com/rancher/wrangler/pkg/yaml"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
+	kyaml "sigs.k8s.io/yaml"
 )
 
 const (
@@ -51,7 +53,7 @@ func Register(ctx context.Context,
 
 func (h *handler) resolveNS(namespace, name string, obj runtime.Object) ([]relatedresource.Key, error) {
 	if _, ok := obj.(*fleet.Cluster); ok {
-		if _, err := h.bundleCache.Get(namespace, agentBundleName); err != nil {
+		if _, err := h.bundleCache.Get(namespace, agentBundleName); err == nil {
 			return []relatedresource.Key{{Name: namespace}}, nil
 		}
 	}
@@ -73,14 +75,11 @@ func (h *handler) OnNamespace(key string, namespace *corev1.Namespace) (*corev1.
 	}
 
 	var objs []runtime.Object
-
-	for _, cluster := range clusters {
-		bundle, err := h.getAgentBundle(namespace.Name, cluster)
-		if err != nil {
-			return nil, err
-		}
-		objs = append(objs, bundle)
+	bundle, err := h.getAgentBundle(namespace.Name, clusters)
+	if err != nil {
+		return nil, err
 	}
+	objs = append(objs, bundle)
 
 	return namespace, h.apply.
 		WithOwner(namespace).
@@ -89,21 +88,28 @@ func (h *handler) OnNamespace(key string, namespace *corev1.Namespace) (*corev1.
 		ApplyObjects(objs...)
 }
 
-func (h *handler) getAgentBundle(ns string, cluster *fleet.Cluster) (runtime.Object, error) {
+func (h *handler) getAgentBundle(ns string, clusters []*fleet.Cluster) (*fleet.Bundle, error) {
 	cfg := config.Get()
 	if cfg.ManageAgent != nil && !*cfg.ManageAgent {
 		return nil, nil
 	}
 
-	objs := agent.Manifest(h.systemNamespace, cfg.AgentImage, cfg.AgentImagePullPolicy, "bundle", cfg.AgentCheckinInternal.Duration.String(), cluster.Spec.AgentEnvVars)
+	objs := agent.Manifest(h.systemNamespace, cfg.AgentImage, cfg.AgentImagePullPolicy, "bundle", cfg.AgentCheckinInternal.Duration.String(), nil)
 	agentYAML, err := yaml.Export(objs...)
 	if err != nil {
 		return nil, err
 	}
+	kustomizeBase := map[string][]string{
+		"resources": {"agent.yaml"},
+	}
+	kustomizeBaseBytes, err := kyaml.Marshal(kustomizeBase)
+	if err != nil {
+		return nil, err
+	}
 
-	return &fleet.Bundle{
+	bundle := &fleet.Bundle{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      name.SafeConcatName(agentBundleName, cluster.Name),
+			Name:      agentBundleName,
 			Namespace: ns,
 		},
 		Spec: fleet.BundleSpec{
@@ -115,23 +121,85 @@ func (h *handler) getAgentBundle(ns string, cluster *fleet.Cluster) (runtime.Obj
 			},
 			Resources: []fleet.BundleResource{
 				{
-					Name:    "agent.yaml",
+					Name:    "base/agent.yaml",
 					Content: string(agentYAML),
 				},
-			},
-			Targets: []fleet.BundleTarget{
 				{
-					ClusterSelector: &metav1.LabelSelector{
-						MatchExpressions: []metav1.LabelSelectorRequirement{
-							{
-								Key:      "fleet.cattle.io/non-managed-agent",
-								Operator: metav1.LabelSelectorOpDoesNotExist,
-							},
-						},
-					},
-					ClusterName: cluster.Name,
+					Name:    "base/kustomization.yaml",
+					Content: string(kustomizeBaseBytes),
 				},
 			},
 		},
-	}, nil
+	}
+
+	sort.Slice(clusters, func(i, j int) bool {
+		return clusters[i].Name < clusters[j].Name
+	})
+
+	for _, cluster := range clusters {
+		target := fleet.BundleTarget{
+			ClusterSelector: &metav1.LabelSelector{
+				MatchExpressions: []metav1.LabelSelectorRequirement{
+					{
+						Key:      "fleet.cattle.io/non-managed-agent",
+						Operator: metav1.LabelSelectorOpDoesNotExist,
+					},
+				},
+			},
+			ClusterName: cluster.Name,
+		}
+		target.Kustomize = &fleet.KustomizeOptions{
+			Dir: cluster.Name,
+		}
+
+		// set up kustomize to add env patch per cluster
+		kustomizeFile := map[string][]string{
+			"resources": {"../base"},
+			"patches":   {"patch.yaml"},
+		}
+		kustomizeFileBytes, err := kyaml.Marshal(kustomizeFile)
+		if err != nil {
+			return nil, err
+		}
+
+		containerPatch := map[string]interface{}{
+			"name": agent.DefaultName,
+			"env":  cluster.Spec.AgentEnvVars,
+		}
+		if len(cluster.Spec.AgentEnvVars) == 0 {
+			delete(containerPatch, "env")
+		}
+		patch := map[string]interface{}{
+			"apiVersion": "apps/v1",
+			"kind":       "Deployment",
+			"metadata": map[string]string{
+				"name":      agent.DefaultName,
+				"namespace": h.systemNamespace,
+			},
+			"spec": map[string]interface{}{
+				"template": map[string]interface{}{
+					"spec": map[string]interface{}{
+						"containers": []map[string]interface{}{
+							containerPatch,
+						},
+					},
+				},
+			},
+		}
+		patchBytes, err := kyaml.Marshal(patch)
+		if err != nil {
+			return nil, err
+		}
+		bundle.Spec.Resources = append(bundle.Spec.Resources, fleet.BundleResource{
+			Name:    fmt.Sprintf("%s/kustomization.yaml", cluster.Name),
+			Content: string(kustomizeFileBytes),
+		}, fleet.BundleResource{
+			Name:    fmt.Sprintf("%s/patch.yaml", cluster.Name),
+			Content: string(patchBytes),
+		})
+
+		bundle.Spec.Targets = append(bundle.Spec.Targets, target)
+	}
+
+	return bundle, nil
 }
