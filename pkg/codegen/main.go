1ec720ddfb2ac909635d233ba1837afa82ca6dd5
Daishan <daishan@rancher.com>
2021-05-21 18:56:26 -0700

    Update wrangler to use crd v1

--- pkg/codegen/main.go
+++ pkg/codegen/main.go
@@ -9,7 +9,7 @@ import (
 	"github.com/rancher/wrangler/pkg/controller-gen/args"
 
 	// Ensure gvk gets loaded in wrangler/pkg/gvk cache
-	_ "github.com/rancher/wrangler/pkg/generated/controllers/apiextensions.k8s.io/v1beta1"
+	_ "github.com/rancher/wrangler/pkg/generated/controllers/apiextensions.k8s.io/v1"
 )
 
 func main() {
