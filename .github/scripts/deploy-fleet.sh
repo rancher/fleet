84fad594f62ce0a66c4f8fd0c181b4511118f7a3
Mario Manno <mario.manno@suse.com>
2022-07-12 10:47:01 +0200

    Add CI for AKS/EKS/GKE clusters
    The workflows are triggered manually.
    Several Github repository secrets are required:
    
    * eks
      * AWS_ACCESS_KEY_ID
      * AWS_SECRET_ACCESS_KEY
      * CI_EKS_SSH_KEY
      * CI_EKS_SSH_PUBKEY
    
    * gked
      * EPCI_GCP_CREDENTIALS
      * EPCI_GKE_PROJECT
      * CI_GKE_SSH_KEY
      * CI_GKE_SSH_PUBKEY
    
    * aks
      * AZURE_CREDENTIALS
      * CI_AKS_SSH_KEY
      * CI_AKS_SSH_PUBKEY
    
    The CI_* secrets are valid (ecdsa) deployment keys for the repository used by
    the 'gitrepo' test.

---
+++ .github/scripts/deploy-fleet.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+# usage: ./deploy-fleet.sh ghcr.io/rancher/fleet:sha-49f6f81 ghcr.io/rancher/fleet-agent:1h
+if [ $# -ge 2 ] && [ -n "$1" ] && [ -n "$2" ]; then
+  fleetRepo="${1%:*}"
+  fleetTag="${1#*:}"
+  agentRepo="${2%:*}"
+  agentTag="${2#*:}"
+else
+  fleetRepo="rancher/fleet"
+  fleetTag="dev"
+  agentRepo="rancher/fleet-agent"
+  agentTag="dev"
+fi
+
+helm -n fleet-system install --create-namespace --wait fleet-crd charts/fleet-crd
+helm upgrade --install fleet charts/fleet \
+  -n fleet-system --create-namespace --wait \
+  --set image.repository="$fleetRepo" \
+  --set image.tag="$fleetTag" \
+  --set agentImage.repository="$agentRepo" \
+  --set agentImage.tag="$agentTag" \
+  --set agentImage.imagePullPolicy=IfNotPresent
+
+kubectl -n fleet-system rollout status deploy/fleet-controller
+{ grep -q -m 1 "fleet-agent"; kill $!; } < <(kubectl get deployment -n fleet-system -w)
+kubectl -n fleet-system rollout status deploy/fleet-agent
