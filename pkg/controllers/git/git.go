9973f975cf2e8fafd6f4d0827dc6568b439fd758
Tim Hardeck <thardeck@suse.com>
2022-09-22 15:16:23 +0200

    Fix parameter generation when using Helm secrets
    (cherry picked from commit 8045e08fcce97145e8eba0c998206696415f2d8d)

--- pkg/controllers/git/git.go
+++ pkg/controllers/git/git.go
@@ -604,7 +604,6 @@ func argsAndEnvs(gitrepo *fleet.GitRepo) ([]string, []corev1.EnvVar) {
 		fmt.Sprintf("--sync-generation=%d", gitrepo.Spec.ForceSyncGeneration),
 		fmt.Sprintf("--paused=%v", gitrepo.Spec.Paused),
 		"--target-namespace", gitrepo.Spec.TargetNamespace,
-		"--",
 	)
 
 	var env []corev1.EnvVar
@@ -638,5 +637,5 @@ func argsAndEnvs(gitrepo *fleet.GitRepo) ([]string, []corev1.EnvVar) {
 			})
 	}
 
-	return append(args, gitrepo.Name), env
+	return append(args, "--", gitrepo.Name), env
 }
