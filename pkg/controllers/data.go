bc065affb5760d0e41476216eba534e415570152
Mario Manno <mario.manno@suse.com>
2022-10-28 16:41:56 +0200

    Reading contents&secrets uses specific service account
    The RBAC previously allowed any user to get contents and any secrets in
    the system registration namespace.
    However, their names are random and listing was not allowed.
    
    The agents on downstream clusters already use the 'request-' account to
    retrieve bundledeployments from the upstream cluster and report back the
    status. We extend that account to be able to retrieve 'contents', too.
    
    The agents use a 'import-' account for reqstration, e.g. to create a
    ClusterRegistration. That one is extended, so it can read the secrets in
    the agent's cluster registration namespace.

--- pkg/controllers/data.go
+++ pkg/controllers/data.go
@@ -57,53 +57,5 @@ func addData(systemNamespace, systemRegistrationNamespace string, appCtx *appCon
 					Name: systemRegistrationNamespace,
 				},
 			},
-			&rbacv1.Role{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "fleet-agent-get-cred",
-					Namespace: systemRegistrationNamespace,
-				},
-				Rules: []rbacv1.PolicyRule{
-					{
-						Verbs:     []string{"get"},
-						APIGroups: []string{""},
-						Resources: []string{"secrets"},
-					},
-				},
-			},
-			&rbacv1.RoleBinding{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "fleet-agent-get-cred",
-					Namespace: systemRegistrationNamespace,
-				},
-				Subjects: []rbacv1.Subject{
-					{
-						Kind:     "Group",
-						APIGroup: "rbac.authorization.k8s.io",
-						Name:     "system:serviceaccounts",
-					},
-				},
-				RoleRef: rbacv1.RoleRef{
-					APIGroup: "rbac.authorization.k8s.io",
-					Kind:     "Role",
-					Name:     "fleet-agent-get-cred",
-				},
-			},
-			&rbacv1.ClusterRoleBinding{
-				ObjectMeta: metav1.ObjectMeta{
-					Name: "fleet-agent-get-content",
-				},
-				Subjects: []rbacv1.Subject{
-					{
-						Kind:     "Group",
-						APIGroup: "rbac.authorization.k8s.io",
-						Name:     "system:serviceaccounts",
-					},
-				},
-				RoleRef: rbacv1.RoleRef{
-					APIGroup: "rbac.authorization.k8s.io",
-					Kind:     "ClusterRole",
-					Name:     "fleet-content",
-				},
-			},
 		)
 }
