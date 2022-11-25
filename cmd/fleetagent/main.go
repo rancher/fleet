33ff4fa7369df983cfa5ca66965b99e82a8442fa
Mario Manno <mario.manno@suse.com>
2022-11-25 16:33:00 +0100

    Add CLI to generate reference docs
    Copied from Epinio.
    Importing 'main' as a package is not possible, so extracting the cobra
    command generation into separate packages, like 'fleet cli' already did.

--- cmd/fleetagent/main.go
+++ cmd/fleetagent/main.go
@@ -2,67 +2,13 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
 	_ "net/http/pprof"
-	"time"
 
-	"github.com/spf13/cobra"
-
-	"github.com/rancher/fleet/modules/agent/pkg/agent"
-	"github.com/rancher/fleet/modules/agent/pkg/simulator"
-	"github.com/rancher/fleet/pkg/version"
+	"github.com/rancher/fleet/modules/agent/cmds"
 
 	command "github.com/rancher/wrangler-cli"
 )
 
-var (
-	debugConfig command.DebugConfig
-)
-
-type FleetAgent struct {
-	Kubeconfig      string `usage:"kubeconfig file"`
-	Namespace       string `usage:"namespace to watch" env:"NAMESPACE"`
-	AgentScope      string `usage:"An identifier used to scope the agent bundleID names, typically the same as namespace" env:"AGENT_SCOPE"`
-	Simulators      int    `usage:"Numbers of simulators to run"`
-	CheckinInterval string `usage:"How often to post cluster status" env:"CHECKIN_INTERVAL"`
-}
-
-func (a *FleetAgent) Run(cmd *cobra.Command, args []string) error {
-	go func() {
-		log.Println(http.ListenAndServe("localhost:6060", nil)) // nolint:gosec // Debugging only
-	}()
-
-	debugConfig.MustSetupDebug()
-	var (
-		opts agent.Options
-		err  error
-	)
-
-	if a.CheckinInterval != "" {
-		opts.CheckinInterval, err = time.ParseDuration(a.CheckinInterval)
-		if err != nil {
-			return err
-		}
-	}
-	if a.Namespace == "" {
-		return fmt.Errorf("--namespace or env NAMESPACE is required to be set")
-	}
-	if a.Simulators > 0 {
-		return simulator.Simulate(cmd.Context(), a.Simulators, a.Kubeconfig, a.Namespace, "default", opts)
-	}
-	if err := agent.Start(cmd.Context(), a.Kubeconfig, a.Namespace, a.AgentScope, &opts); err != nil {
-		return err
-	}
-	<-cmd.Context().Done()
-	return nil
-}
-
 func main() {
-	cmd := command.Command(&FleetAgent{}, cobra.Command{
-		Version: version.FriendlyVersion(),
-	})
-	cmd = command.AddDebug(cmd, &debugConfig)
-	command.Main(cmd)
+	command.Main(cmds.App())
 }
--- cmd/fleetcontroller/main.go
+++ cmd/fleetcontroller/main.go
@@ -2,104 +2,15 @@
 package main
 
 import (
-	"context"
-	"log"
-	"net/http"
 	_ "net/http/pprof"
-	"os"
-	"path"
-	"runtime/pprof"
-	"time"
 
-	"github.com/rancher/fleet/pkg/durations"
-	"k8s.io/apimachinery/pkg/util/wait"
-
-	"github.com/spf13/cobra"
-
-	"github.com/rancher/fleet/pkg/agent"
-	"github.com/rancher/fleet/pkg/fleetcontroller"
-	"github.com/rancher/fleet/pkg/version"
+	controller "github.com/rancher/fleet/modules/controller/cmds"
 
 	command "github.com/rancher/wrangler-cli"
 	_ "github.com/rancher/wrangler/pkg/generated/controllers/apiextensions.k8s.io"
 	_ "github.com/rancher/wrangler/pkg/generated/controllers/networking.k8s.io"
 )
 
-var (
-	debugConfig command.DebugConfig
-)
-
-type FleetManager struct {
-	Kubeconfig    string `usage:"Kubeconfig file"`
-	Namespace     string `usage:"namespace to watch" default:"cattle-fleet-system" env:"NAMESPACE"`
-	DisableGitops bool   `usage:"disable gitops components" name:"disable-gitops"`
-}
-
-func (f *FleetManager) Run(cmd *cobra.Command, args []string) error {
-	setupCpuPprof(cmd.Context())
-	go func() {
-		log.Println(http.ListenAndServe("localhost:6060", nil)) // nolint:gosec // Debugging only
-	}()
-	debugConfig.MustSetupDebug()
-	if err := fleetcontroller.Start(cmd.Context(), f.Namespace, f.Kubeconfig, f.DisableGitops); err != nil {
-		return err
-	}
-
-	if debugConfig.Debug {
-		agent.DebugLevel = debugConfig.DebugLevel
-	}
-
-	<-cmd.Context().Done()
-	return nil
-}
-
-// setupCpuPprof starts a goroutine that captures a cpu pprof profile
-// into FLEET_CPU_PPROF_DIR every FLEET_CPU_PPROF_PERIOD
-func setupCpuPprof(ctx context.Context) {
-	if dir, ok := os.LookupEnv("FLEET_CPU_PPROF_DIR"); ok {
-		go func() {
-			var pprofCpuFile *os.File
-
-			period := durations.DefaultCpuPprofPeriod
-			if customPeriod, err := time.ParseDuration(os.Getenv("FLEET_CPU_PPROF_PERIOD")); err == nil {
-				period = customPeriod
-			}
-			wait.UntilWithContext(ctx, func(ctx context.Context) {
-				stopCpuPprof(pprofCpuFile)
-				pprofCpuFile = startCpuPprof(dir)
-			}, period)
-		}()
-	}
-}
-
-// stopCpuPprof concludes a cpu pprof capture, if any is ongoing
-func stopCpuPprof(f *os.File) {
-	pprof.StopCPUProfile()
-	if f != nil {
-		err := f.Close()
-		if err != nil {
-			log.Println("could not close CPU profile file ", err)
-		}
-	}
-}
-
-// startCpuPprof starts a pprof cpu capture into a timestamp-prefixed file in dir
-func startCpuPprof(dir string) *os.File {
-	name := time.Now().UTC().Format("2006-01-02_15_04_05") + ".pprof.fleetcontroller.samples.cpu.pb.gz"
-	f, err := os.Create(path.Join(dir, name))
-	if err != nil {
-		log.Println("could not create CPU profile: ", err)
-	}
-	if err := pprof.StartCPUProfile(f); err != nil {
-		log.Println("could not start CPU profile: ", err)
-	}
-	return f
-}
-
 func main() {
-	cmd := command.Command(&FleetManager{}, cobra.Command{
-		Version: version.FriendlyVersion(),
-	})
-	cmd = command.AddDebug(cmd, &debugConfig)
-	command.Main(cmd)
+	command.Main(controller.App())
 }
