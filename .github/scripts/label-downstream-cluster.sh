f365da63fc765db4cdf5fc0bc7fd98ba1f16d72a
Mario Manno <mario.manno@suse.com>
2023-01-11 12:09:50 +0100

    Configurable namespaces in dev scripts
    Rancher 2.5 uses different namespaces.

--- .github/scripts/label-downstream-cluster.sh
+++ .github/scripts/label-downstream-cluster.sh
@@ -2,6 +2,8 @@
 
 set -euxo pipefail
 
-{ grep -q -m 1 -e "k3d-downstream"; kill $!; } < <(kubectl get clusters.fleet.cattle.io -n ${FLEET_E2E_NS-fleet-default} -w)
-name=$(kubectl get clusters.fleet.cattle.io -o=jsonpath='{.items[0].metadata.name}' -n ${FLEET_E2E_NS-fleet-default})
-kubectl patch clusters.fleet.cattle.io -n ${FLEET_E2E_NS-fleet-default} "$name" --type=json -p '[{"op": "add", "path": "/metadata/labels/env", "value": "test" }]'
+ns=${FLEET_E2E_NS-fleet-default}
+
+{ grep -q -m 1 -e "k3d-downstream"; kill $!; } < <(kubectl get clusters.fleet.cattle.io -n "$ns" -w)
+name=$(kubectl get clusters.fleet.cattle.io -o=jsonpath='{.items[0].metadata.name}' -n "$ns")
+kubectl patch clusters.fleet.cattle.io -n "$ns" "$name" --type=json -p '[{"op": "add", "path": "/metadata/labels/env", "value": "test" }]'
