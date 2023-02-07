550c35dc260f6b2389eaa76acc0e01b7336f0909
Tim Hardeck <thardeck@suse.com>
2023-02-07 17:34:58 +0100

    Do not add managed label instead of the annotation
    for the namespace object.

--- pkg/agent/agent.go
+++ pkg/agent/agent.go
@@ -105,9 +105,6 @@ func agentToken(ctx context.Context, agentNamespace, controllerNamespace string,
 		&v1.Namespace{
 			ObjectMeta: metav1.ObjectMeta{
 				Name: agentNamespace,
-				Labels: map[string]string{
-					fleet.ManagedLabel: "true",
-				},
 			},
 		},
 		// fleet-agent-bootstrap
