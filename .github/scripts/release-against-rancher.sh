63cf3ef1808693939dcf3a7cf2487e9bd3391a97
Tim Hardeck <thardeck@suse.com>
2023-02-23 10:31:08 +0100

    Add Github action to submit Fleet to Rancher

---
+++ .github/scripts/release-against-rancher.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#
+# Submit new Fleet version against rancher/rancher
+
+set -uex
+
+PREV_FLEET_VERSION=$1   # e.g. 0.6.0-rc.3
+NEW_FLEET_VERSION=$2
+PREV_CHART_VERSION=$3   # e.g. 101.1.0
+NEW_CHART_VERSION=$4
+BUMP_API=$5             # bump api if `true`
+
+bump_fleet_api() {
+    COMMIT=$1
+
+    go get -u "github.com/rancher/fleet/pkg/apis@${COMMIT}"
+    go mod tidy
+}
+
+if [ -z "${GITHUB_WORKSPACE:-}" ]; then
+    CHARTS_DIR="$(dirname -- "$0")/../../../rancher"
+else
+    CHARTS_DIR="${GITHUB_WORKSPACE}/rancher"
+fi
+
+pushd "${CHARTS_DIR}" > /dev/null
+
+if [ ! -e ~/.gitconfig ]; then
+    git config --global user.name "fleet-bot"
+    git config --global user.email fleet@suse.de
+fi
+
+sed -i -e "s/ENV CATTLE_FLEET_MIN_VERSION=${PREV_CHART_VERSION}+up${PREV_FLEET_VERSION}/ENV CATTLE_FLEET_MIN_VERSION=${NEW_CHART_VERSION}+up${NEW_FLEET_VERSION}/g" package/Dockerfile
+
+git add package/Dockerfile
+
+if [ "${BUMP_API}" == "true" ]; then
+    pushd ../fleet > /dev/null
+        COMMIT=$(git rev-list -n 1 "v${NEW_FLEET_VERSION}")
+    popd > /dev/null
+
+    bump_fleet_api "${COMMIT}"
+
+    pushd pkg/apis > /dev/null
+        bump_fleet_api "${COMMIT}"
+    popd > /dev/null
+
+    git add go.* pkg/apis/go.*
+fi
+
+git commit -m "Updating to Fleet v${NEW_FLEET_VERSION}"
+
+popd > /dev/null
