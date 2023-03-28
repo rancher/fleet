a02f65c3de997d7a4a9e1c81d745c5592d0a6658
Tim Hardeck <thardeck@suse.com>
2023-03-28 14:53:06 +0200

    Use echo instead of printf in bash script

--- .github/scripts/check-for-auto-generated-changes.sh
+++ .github/scripts/check-for-auto-generated-changes.sh
@@ -7,9 +7,9 @@ go run pkg/codegen/main.go
 go run ./pkg/codegen crds ./charts/fleet-crd/templates/crds.yaml > /dev/null
 
 if [ -n "$(git status --porcelain)" ]; then
-    printf "Generated files have either been changed manually or were not updated.\n"
-
-    printf "The following generated files did differ after regeneration:"
+    echo "Generated files have either been changed manually or were not updated."
+    echo
+    echo "The following generated files did differ after regeneration:"
     git status --porcelain
     exit 1
 fi
