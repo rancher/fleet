d11b5e9da0af05c01a741ee59507f89e7741c21d
Silvio Moioli <silvio@moioli.net>
2023-04-27 15:47:30 +0200

    fleet-agent: do not attempt re-queuing if k8s version is not compatible
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- modules/agent/pkg/controllers/bundledeployment/controller.go
+++ modules/agent/pkg/controllers/bundledeployment/controller.go
@@ -152,7 +152,8 @@ func deployErrToStatus(err error, status fleet.BundleDeploymentStatus) (bool, fl
 			"(failed, and has been rolled back due to atomic being set)|" + // atomic is set and a rollback occurs
 			"(YAML parse error)|" + // YAML is broken in source files
 			"(Forbidden: updates to [0-9A-Za-z]+ spec for fields other than [0-9A-Za-z ']+ are forbidden)|" + // trying to update fields that cannot be updated
-			"(Forbidden: spec is immutable after creation)", // trying to modify immutable spec
+			"(Forbidden: spec is immutable after creation)|" + // trying to modify immutable spec
+			"(chart requires kubeVersion: [0-9A-Za-z\\.\\-<>=]+ which is incompatible with Kubernetes)", // trying to deploy to incompatible Kubernetes
 	)
 	if re.MatchString(msg) {
 		status.Ready = false
