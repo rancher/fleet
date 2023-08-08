388cf7703a9e675cb0076cf40fb01ac37e09d670
Mario Manno <mario.manno@suse.com>
2023-08-08 14:13:17 +0200

    Update internal/cmd/cli/cleanup.go
    Co-authored-by: Tim Hardeck <thardeck@suse.com>

--- internal/cmd/cli/cleanup.go
+++ internal/cmd/cli/cleanup.go
@@ -37,7 +37,7 @@ func (a *CleanUp) Run(cmd *cobra.Command, args []string) error {
 			return err
 		}
 		if min <= 0 {
-			return errors.New("min cannot be negative")
+			return errors.New("min cannot be zero or less")
 		}
 	}
 
