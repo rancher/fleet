6dc651ca1f5a16396319f2f3b6fe5c18948ab91a
Alejandro Ruiz <alejandro.ruiz@suse.com>
2023-09-07 16:26:30 +0200

    test: add e2e tests for SkipSchemaValidation option

--- e2e/single-cluster/helm_options_test.go
+++ e2e/single-cluster/helm_options_test.go
@@ -50,4 +50,28 @@ var _ = Describe("Helm deploy options", func() {
 			})
 		})
 	})
+
+	Describe("SkipSchemaValidation", func() {
+		BeforeEach(func() {
+			asset = "single-cluster/helm-options-skip-schema-validation.yaml"
+		})
+		When("is false", func() {
+			bundleName := "helm-options-skip-schema-val-helm-schemas-not-set"
+			It("fails when schema validation does not pass", func() {
+				Eventually(func() string {
+					out, _ := k.Get("bundle", bundleName, `-o=jsonpath='{.status.conditions[?(@.type=="Ready")].message}'`)
+					return out
+				}).Should(ContainSubstring("values don't meet the specifications of the schema"))
+			})
+		})
+		When("is true", func() {
+			bundleName := "helm-options-skip-schema-val-helm-schemas-set"
+			It("completely skips the schema validation", func() {
+				Eventually(func() string {
+					out, _ := k.Get("bundle", bundleName, `-o=jsonpath={.status.conditions[?(@.type=="Ready")].status}`)
+					return out
+				}).Should(Equal("True"))
+			})
+		})
+	})
 })
