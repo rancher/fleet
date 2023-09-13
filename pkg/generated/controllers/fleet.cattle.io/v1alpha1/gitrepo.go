98e85edd4248a36c83e548ee609dcbd33f2771df
Alejandro Ruiz <alejandro.ruiz@suse.com>
2023-09-13 14:03:22 +0200

    build: run go generate

--- pkg/generated/controllers/fleet.cattle.io/v1alpha1/gitrepo.go
+++ pkg/generated/controllers/fleet.cattle.io/v1alpha1/gitrepo.go
@@ -23,244 +23,29 @@ import (
 	"time"
 
 	v1alpha1 "github.com/rancher/fleet/pkg/apis/fleet.cattle.io/v1alpha1"
-	"github.com/rancher/lasso/pkg/client"
-	"github.com/rancher/lasso/pkg/controller"
 	"github.com/rancher/wrangler/pkg/apply"
 	"github.com/rancher/wrangler/pkg/condition"
 	"github.com/rancher/wrangler/pkg/generic"
 	"github.com/rancher/wrangler/pkg/kv"
 	"k8s.io/apimachinery/pkg/api/equality"
 	"k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/apimachinery/pkg/types"
-	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
-	"k8s.io/apimachinery/pkg/watch"
-	"k8s.io/client-go/tools/cache"
 )
 
-type GitRepoHandler func(string, *v1alpha1.GitRepo) (*v1alpha1.GitRepo, error)
-
+// GitRepoController interface for managing GitRepo resources.
 type GitRepoController interface {
-	generic.ControllerMeta
-	GitRepoClient
-
-	OnChange(ctx context.Context, name string, sync GitRepoHandler)
-	OnRemove(ctx context.Context, name string, sync GitRepoHandler)
-	Enqueue(namespace, name string)
-	EnqueueAfter(namespace, name string, duration time.Duration)
-
-	Cache() GitRepoCache
+	generic.ControllerInterface[*v1alpha1.GitRepo, *v1alpha1.GitRepoList]
 }
 
+// GitRepoClient interface for managing GitRepo resources in Kubernetes.
 type GitRepoClient interface {
-	Create(*v1alpha1.GitRepo) (*v1alpha1.GitRepo, error)
-	Update(*v1alpha1.GitRepo) (*v1alpha1.GitRepo, error)
-	UpdateStatus(*v1alpha1.GitRepo) (*v1alpha1.GitRepo, error)
-	Delete(namespace, name string, options *metav1.DeleteOptions) error
-	Get(namespace, name string, options metav1.GetOptions) (*v1alpha1.GitRepo, error)
-	List(namespace string, opts metav1.ListOptions) (*v1alpha1.GitRepoList, error)
-	Watch(namespace string, opts metav1.ListOptions) (watch.Interface, error)
-	Patch(namespace, name string, pt types.PatchType, data []byte, subresources ...string) (result *v1alpha1.GitRepo, err error)
+	generic.ClientInterface[*v1alpha1.GitRepo, *v1alpha1.GitRepoList]
 }
 
+// GitRepoCache interface for retrieving GitRepo resources in memory.
 type GitRepoCache interface {
-	Get(namespace, name string) (*v1alpha1.GitRepo, error)
-	List(namespace string, selector labels.Selector) ([]*v1alpha1.GitRepo, error)
-
-	AddIndexer(indexName string, indexer GitRepoIndexer)
-	GetByIndex(indexName, key string) ([]*v1alpha1.GitRepo, error)
-}
-
-type GitRepoIndexer func(obj *v1alpha1.GitRepo) ([]string, error)
-
-type gitRepoController struct {
-	controller    controller.SharedController
-	client        *client.Client
-	gvk           schema.GroupVersionKind
-	groupResource schema.GroupResource
-}
-
-func NewGitRepoController(gvk schema.GroupVersionKind, resource string, namespaced bool, controller controller.SharedControllerFactory) GitRepoController {
-	c := controller.ForResourceKind(gvk.GroupVersion().WithResource(resource), gvk.Kind, namespaced)
-	return &gitRepoController{
-		controller: c,
-		client:     c.Client(),
-		gvk:        gvk,
-		groupResource: schema.GroupResource{
-			Group:    gvk.Group,
-			Resource: resource,
-		},
-	}
-}
-
-func FromGitRepoHandlerToHandler(sync GitRepoHandler) generic.Handler {
-	return func(key string, obj runtime.Object) (ret runtime.Object, err error) {
-		var v *v1alpha1.GitRepo
-		if obj == nil {
-			v, err = sync(key, nil)
-		} else {
-			v, err = sync(key, obj.(*v1alpha1.GitRepo))
-		}
-		if v == nil {
-			return nil, err
-		}
-		return v, err
-	}
-}
-
-func (c *gitRepoController) Updater() generic.Updater {
-	return func(obj runtime.Object) (runtime.Object, error) {
-		newObj, err := c.Update(obj.(*v1alpha1.GitRepo))
-		if newObj == nil {
-			return nil, err
-		}
-		return newObj, err
-	}
-}
-
-func UpdateGitRepoDeepCopyOnChange(client GitRepoClient, obj *v1alpha1.GitRepo, handler func(obj *v1alpha1.GitRepo) (*v1alpha1.GitRepo, error)) (*v1alpha1.GitRepo, error) {
-	if obj == nil {
-		return obj, nil
-	}
-
-	copyObj := obj.DeepCopy()
-	newObj, err := handler(copyObj)
-	if newObj != nil {
-		copyObj = newObj
-	}
-	if obj.ResourceVersion == copyObj.ResourceVersion && !equality.Semantic.DeepEqual(obj, copyObj) {
-		return client.Update(copyObj)
-	}
-
-	return copyObj, err
-}
-
-func (c *gitRepoController) AddGenericHandler(ctx context.Context, name string, handler generic.Handler) {
-	c.controller.RegisterHandler(ctx, name, controller.SharedControllerHandlerFunc(handler))
-}
-
-func (c *gitRepoController) AddGenericRemoveHandler(ctx context.Context, name string, handler generic.Handler) {
-	c.AddGenericHandler(ctx, name, generic.NewRemoveHandler(name, c.Updater(), handler))
-}
-
-func (c *gitRepoController) OnChange(ctx context.Context, name string, sync GitRepoHandler) {
-	c.AddGenericHandler(ctx, name, FromGitRepoHandlerToHandler(sync))
-}
-
-func (c *gitRepoController) OnRemove(ctx context.Context, name string, sync GitRepoHandler) {
-	c.AddGenericHandler(ctx, name, generic.NewRemoveHandler(name, c.Updater(), FromGitRepoHandlerToHandler(sync)))
-}
-
-func (c *gitRepoController) Enqueue(namespace, name string) {
-	c.controller.Enqueue(namespace, name)
-}
-
-func (c *gitRepoController) EnqueueAfter(namespace, name string, duration time.Duration) {
-	c.controller.EnqueueAfter(namespace, name, duration)
-}
-
-func (c *gitRepoController) Informer() cache.SharedIndexInformer {
-	return c.controller.Informer()
-}
-
-func (c *gitRepoController) GroupVersionKind() schema.GroupVersionKind {
-	return c.gvk
-}
-
-func (c *gitRepoController) Cache() GitRepoCache {
-	return &gitRepoCache{
-		indexer:  c.Informer().GetIndexer(),
-		resource: c.groupResource,
-	}
-}
-
-func (c *gitRepoController) Create(obj *v1alpha1.GitRepo) (*v1alpha1.GitRepo, error) {
-	result := &v1alpha1.GitRepo{}
-	return result, c.client.Create(context.TODO(), obj.Namespace, obj, result, metav1.CreateOptions{})
-}
-
-func (c *gitRepoController) Update(obj *v1alpha1.GitRepo) (*v1alpha1.GitRepo, error) {
-	result := &v1alpha1.GitRepo{}
-	return result, c.client.Update(context.TODO(), obj.Namespace, obj, result, metav1.UpdateOptions{})
-}
-
-func (c *gitRepoController) UpdateStatus(obj *v1alpha1.GitRepo) (*v1alpha1.GitRepo, error) {
-	result := &v1alpha1.GitRepo{}
-	return result, c.client.UpdateStatus(context.TODO(), obj.Namespace, obj, result, metav1.UpdateOptions{})
-}
-
-func (c *gitRepoController) Delete(namespace, name string, options *metav1.DeleteOptions) error {
-	if options == nil {
-		options = &metav1.DeleteOptions{}
-	}
-	return c.client.Delete(context.TODO(), namespace, name, *options)
-}
-
-func (c *gitRepoController) Get(namespace, name string, options metav1.GetOptions) (*v1alpha1.GitRepo, error) {
-	result := &v1alpha1.GitRepo{}
-	return result, c.client.Get(context.TODO(), namespace, name, result, options)
-}
-
-func (c *gitRepoController) List(namespace string, opts metav1.ListOptions) (*v1alpha1.GitRepoList, error) {
-	result := &v1alpha1.GitRepoList{}
-	return result, c.client.List(context.TODO(), namespace, result, opts)
-}
-
-func (c *gitRepoController) Watch(namespace string, opts metav1.ListOptions) (watch.Interface, error) {
-	return c.client.Watch(context.TODO(), namespace, opts)
-}
-
-func (c *gitRepoController) Patch(namespace, name string, pt types.PatchType, data []byte, subresources ...string) (*v1alpha1.GitRepo, error) {
-	result := &v1alpha1.GitRepo{}
-	return result, c.client.Patch(context.TODO(), namespace, name, pt, data, result, metav1.PatchOptions{}, subresources...)
-}
-
-type gitRepoCache struct {
-	indexer  cache.Indexer
-	resource schema.GroupResource
-}
-
-func (c *gitRepoCache) Get(namespace, name string) (*v1alpha1.GitRepo, error) {
-	obj, exists, err := c.indexer.GetByKey(namespace + "/" + name)
-	if err != nil {
-		return nil, err
-	}
-	if !exists {
-		return nil, errors.NewNotFound(c.resource, name)
-	}
-	return obj.(*v1alpha1.GitRepo), nil
-}
-
-func (c *gitRepoCache) List(namespace string, selector labels.Selector) (ret []*v1alpha1.GitRepo, err error) {
-
-	err = cache.ListAllByNamespace(c.indexer, namespace, selector, func(m interface{}) {
-		ret = append(ret, m.(*v1alpha1.GitRepo))
-	})
-
-	return ret, err
-}
-
-func (c *gitRepoCache) AddIndexer(indexName string, indexer GitRepoIndexer) {
-	utilruntime.Must(c.indexer.AddIndexers(map[string]cache.IndexFunc{
-		indexName: func(obj interface{}) (strings []string, e error) {
-			return indexer(obj.(*v1alpha1.GitRepo))
-		},
-	}))
-}
-
-func (c *gitRepoCache) GetByIndex(indexName, key string) (result []*v1alpha1.GitRepo, err error) {
-	objs, err := c.indexer.ByIndex(indexName, key)
-	if err != nil {
-		return nil, err
-	}
-	result = make([]*v1alpha1.GitRepo, 0, len(objs))
-	for _, obj := range objs {
-		result = append(result, obj.(*v1alpha1.GitRepo))
-	}
-	return result, nil
+	generic.CacheInterface[*v1alpha1.GitRepo]
 }
 
 type GitRepoStatusHandler func(obj *v1alpha1.GitRepo, status v1alpha1.GitRepoStatus) (v1alpha1.GitRepoStatus, error)
@@ -273,7 +58,7 @@ func RegisterGitRepoStatusHandler(ctx context.Context, controller GitRepoControl
 		condition: condition,
 		handler:   handler,
 	}
-	controller.AddGenericHandler(ctx, name, FromGitRepoHandlerToHandler(statusHandler.sync))
+	controller.AddGenericHandler(ctx, name, generic.FromObjectHandlerToHandler(statusHandler.sync))
 }
 
 func RegisterGitRepoGeneratingHandler(ctx context.Context, controller GitRepoController, apply apply.Apply,
