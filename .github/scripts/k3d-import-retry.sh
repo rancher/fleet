cce35be2970a2207f7aa513d6d3c720b4d2d585e
Mario Manno <mario.manno@suse.com>
2023-09-19 13:24:04 +0200

    Try to detect error instead

--- .github/scripts/k3d-import-retry.sh
+++ .github/scripts/k3d-import-retry.sh
@@ -2,21 +2,13 @@
 
 set -euxo pipefail
 
-container=${1:-k3d-upstream}
-name=${2:-fleet}
-shift
-shift
-
-echo "Run k3d import with \"$*\" and retry if '$name' is not found in 'critcl images' output"
-
 i=0
-while ! ( docker exec "$container"-server-0 /bin/crictl images | grep -q "$name" ); do
+while k3d image import "$@" -m direct | grep -q "failed to import"; do
   i=$((i + 1))
   if (( i > 3 )); then
     echo "failed to import images"
     exit 1
   fi
-  k3d image import "$@"
-  # crictl images doesn't show the image immediately after import
-  sleep 20
+  echo "retrying... $i"
+  sleep 1
 done
