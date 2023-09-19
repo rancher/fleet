c8777565f17bc5b9e9b08dcc377dcd123801087b
Mario Manno <mario.manno@suse.com>
2023-09-19 13:34:43 +0200

    fixup! Try to detect error instead

--- .github/scripts/k3d-import-retry.sh
+++ .github/scripts/k3d-import-retry.sh
@@ -3,7 +3,8 @@
 set -euxo pipefail
 
 i=0
-while k3d image import "$@" -m direct | grep -q "failed to import"; do
+# direct mode exits with 1 in case of an error, tool mode doesn't
+while ! k3d image import "$@" -m direct; do
   i=$((i + 1))
   if (( i > 3 )); then
     echo "failed to import images"
