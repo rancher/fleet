52aaf49e12277b367a307b0e796949984331e1e3
Mario Manno <mario.manno@suse.com>
2023-09-21 12:08:29 +0200

    Switch k3d import to default mode to detect error in e2e setup

--- .github/scripts/k3d-import-retry.sh
+++ .github/scripts/k3d-import-retry.sh
@@ -3,8 +3,9 @@
 set -euxo pipefail
 
 i=0
-# direct mode exits with 1 in case of an error, tool mode doesn't
-while ! k3d image import "$@" -m direct; do
+# "Tool" mode doesn't exit with 1 in case of an error, direct mode may freeze.
+# Run tool mode and try to detect the error message in its output:
+while k3d image import "$@" 2>&1 | grep -q "failed to import"; do
   i=$((i + 1))
   if (( i > 3 )); then
     echo "failed to import images"
