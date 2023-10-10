c2d3404103f2708e9aac78a45b34ec730d3d9ec0
Corentin Néau <tan.neau@suse.com>
2023-10-10 14:09:25 +0200

    Increase granularity of infra setup/teardown commands
    Different parts of the test infra can now be set up and torn down
    independently by means of new flags:
    * `chart-museum`
    * `git-server`
    * `helm-registry`
    For instance, `infra setup --git-server=true` can be used to set up only
    the git server.
    If none of those new flags is set, the setup and teardown commands
    behave as they already did, by setting up, respectively tearing down all
    3 components.

--- e2e/testenv/infra/cmd/teardown.go
+++ e2e/testenv/infra/cmd/teardown.go
@@ -10,7 +10,7 @@ import (
 
 // teardownCmd represents the teardown command
 var teardownCmd = &cobra.Command{
-	Use:   "teardown",
+	Use:   "teardown [--git-server=(true|false)|--helm-registry=(true|false)|--chart-museum=(true|false)]",
 	Short: "Tear down an end-to-end test environment",
 	Long:  `This tears down the git server, Helm registry and associated resources needed to run end-to-end tests.`,
 	Run: func(cmd *cobra.Command, args []string) {
@@ -19,23 +19,34 @@ var teardownCmd = &cobra.Command{
 		env := testenv.New()
 		k := env.Kubectl.Namespace(env.Namespace)
 
+		// Only act on specified components, unless none is specified in which case all are affected.
+		if !withGitServer && !withHelmRegistry && !withChartMuseum {
+			withGitServer, withHelmRegistry, withChartMuseum = true, true, true
+		}
+
 		_, _ = k.Delete("gitrepo", "helm")
-		_, _ = k.Delete("deployment", "git-server")
-		_, _ = k.Delete("service", "git-service")
 
-		_, _ = k.Delete("configmap", "zot-config")
-		_, _ = k.Delete("deployment", "zot")
-		_, _ = k.Delete("service", "zot-service")
-		_, _ = k.Delete("secret", "zot-htpasswd")
+		if withGitServer {
+			_, _ = k.Delete("deployment", "git-server")
+			_, _ = k.Delete("service", "git-service")
+		}
 
-		_, _ = k.Delete("deployment", "chartmuseum")
-		_, _ = k.Delete("service", "chartmuseum-service")
+		if withHelmRegistry {
+			_, _ = k.Delete("configmap", "zot-config")
+			_, _ = k.Delete("deployment", "zot")
+			_, _ = k.Delete("service", "zot-service")
+			_, _ = k.Delete("secret", "zot-htpasswd")
+		}
 
-		_, _ = k.Delete("secret", "helm-tls")
-		_, _ = k.Delete("secret", "helm-secret")
+		if withChartMuseum {
+			_, _ = k.Delete("deployment", "chartmuseum")
+			_, _ = k.Delete("service", "chartmuseum-service")
+		}
+
+		if withHelmRegistry && withChartMuseum {
+			_, _ = k.Delete("secret", "helm-tls")
+			_, _ = k.Delete("secret", "helm-secret")
+		}
+
 	},
-}
-
-func init() {
-	rootCmd.AddCommand(teardownCmd)
 }
