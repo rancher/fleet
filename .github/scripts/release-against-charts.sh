4f339567a5d7349ef9342f03f29c66e3898dcc19
Corentin Néau <tan.neau@suse.com>
2023-11-24 17:13:40 +0100

    [TEST] Customise new chart version URL
    When testing a Fleet commit against Rancher, one does not want to
    release that commit as an official `rancher/fleet` release.
    Instead, in such cases `scripts/release-against-charts.sh` edits
    `package.yaml` for Fleet charts to set a URL to a chart built from that
    commit.
    
    Left to do:
    * check: does this work with a local URL?
    * if not, where to push temporary builds?

--- .github/scripts/release-against-charts.sh
+++ .github/scripts/release-against-charts.sh
@@ -9,6 +9,7 @@ NEW_FLEET_VERSION="$2"
 PREV_CHART_VERSION="$3"   # e.g. 101.2.0
 NEW_CHART_VERSION="$4"
 REPLACE="$5"              # remove previous version if `true`, otherwise add new
+CHART_URL="$6" # source chart from a different URL for testing purposes
 
 CHARTS_DIR=${CHARTS_DIR-"$(dirname -- "$0")/../../../charts"}
 
@@ -24,8 +25,16 @@ if [ ! -f bin/charts-build-scripts ]; then
 fi
 
 if grep -q "version: ${PREV_CHART_VERSION}" ./packages/fleet/fleet/package.yaml && grep -q "${PREV_FLEET_VERSION}" ./packages/fleet/fleet/package.yaml; then
+
     find ./packages/fleet/ -type f -exec sed -i -e "s/${PREV_FLEET_VERSION}/${NEW_FLEET_VERSION}/g" {} \;
     find ./packages/fleet/ -type f -exec sed -i -e "s/version: ${PREV_CHART_VERSION}/version: ${NEW_CHART_VERSION}/g" {} \;
+
+    if [[ $# -eq 6 && -n $CHART_URL ]]; then
+        echo "### Custom chart URL: $CHART_URL"
+        find ./packages/fleet/ -type f -name 'package.yaml' -exec sed -i -e "s,^url:.*${NEW_FLEET_VERSION},url: $CHART_URL," {} \;
+        # DEBUG
+        find ./packages/fleet/ -type f -name 'package.yaml' -exec cat {} \;
+    fi
 else
     echo "Previous Fleet version references do not exist in ./packages/fleet/ so replacing it with the new version is not possible. Exiting..."
     exit 1
