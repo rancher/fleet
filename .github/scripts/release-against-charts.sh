0c4b6fec60e2b741258a62eeab2da88ff5a0e854
Corentin Néau <tan.neau@suse.com>
2023-11-29 18:05:38 +0100

    Test: fix replace string in package.yaml
    This should reflect new behaviour from the test release CI workflow,
    which copies YAML directories instead of archives. `packages/*` URLs are
    not compatible with archives; `rancher/charts-build-scripts` expects
    directories in such cases.

--- .github/scripts/release-against-charts.sh
+++ .github/scripts/release-against-charts.sh
@@ -31,7 +31,7 @@ if grep -q "version: ${PREV_CHART_VERSION}" ./packages/fleet/fleet/package.yaml 
 
     if [[ $# -eq 6 && -n $CHART_URL ]]; then
         echo "### Custom chart URL: $CHART_URL"
-        find ./packages/fleet/ -type f -name 'package.yaml' -exec sed -i -e "s/${PREV_FLEET_VERSION}/${NEW_FLEET_VERSION}/g;s,url: .*/,url: $CHART_URL/," {} \;
+        find ./packages/fleet/ -type f -name 'package.yaml' -exec sed -i -e "s,url: .*/,url: $CHART_URL/,;s/-${NEW_FLEET_VERSION}.tgz//" {} \;
 
         # DEBUG
         find ./packages/fleet/ -type f -name 'package.yaml' -exec cat {} \;
