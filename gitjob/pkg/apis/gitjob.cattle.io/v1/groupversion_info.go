0c6b5e404c69599a4919cfbce2c6aa96c2674e42
Tim Hardeck <thardeck@suse.com>
2024-01-02 16:01:52 +0100

    Bump copyright statement to 2024

--- gitjob/pkg/apis/gitjob.cattle.io/v1/groupversion_info.go
+++ gitjob/pkg/apis/gitjob.cattle.io/v1/groupversion_info.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2021-2023 SUSE LLC
+// Copyright (c) 2021-2024 SUSE LLC
 
 // Package v1alpha1 contains API Schema definitions for the fleet.cattle.io v1alpha1 API group
 // +kubebuilder:object:generate=true
