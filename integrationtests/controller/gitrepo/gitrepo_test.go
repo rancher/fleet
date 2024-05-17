0b58b7bf68b6b0099a01b3398ae4a2af8a377130
Corentin Néau <tan.neau@suse.com>
2024-05-17 12:43:26 +0200

    Randomise gitrepo naming in gitrepo integration tests
    This brings consistency with other test cases. Still, this does not seem
    to resolve conflicts at `GitRepo` creation time, which seem to be caused
    by limitations of the `setenv` framework.
    This commit also skips deletion of the test namespace, due to those
    limitations [1].
    
    [1]: https://book.kubebuilder.io/reference/envtest#namespace-usage-limitation

--- integrationtests/controller/gitrepo/gitrepo_test.go
+++ integrationtests/controller/gitrepo/gitrepo_test.go
@@ -1,6 +1,10 @@
 package gitrepo
 
 import (
+	"encoding/hex"
+	"fmt"
+	"math/rand"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
@@ -20,18 +24,24 @@ var _ = Describe("GitRepo", Ordered, func() {
 		Expect(err).ToNot(HaveOccurred())
 		ns := &corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: namespace}}
 		Expect(k8sClient.Create(ctx, ns)).ToNot(HaveOccurred())
-
-		DeferCleanup(func() {
-			Expect(k8sClient.Delete(ctx, ns)).ToNot(HaveOccurred())
-		})
 	})
 
-	var gitrepo *v1alpha1.GitRepo
+	var (
+		gitrepo     *v1alpha1.GitRepo
+		gitrepoName string
+	)
 
 	BeforeEach(func() {
+		p := make([]byte, 12)
+		s := rand.New(rand.NewSource(GinkgoRandomSeed())) // nolint:gosec // non-crypto usage
+		if _, err := s.Read(p); err != nil {
+			panic(err)
+		}
+		gitrepoName = fmt.Sprintf("test-gitrepo-%.12s", hex.EncodeToString(p))
+
 		gitrepo = &v1alpha1.GitRepo{
 			ObjectMeta: metav1.ObjectMeta{
-				Name:      "test-gitrepo",
+				Name:      gitrepoName,
 				Namespace: namespace,
 			},
 			Spec: v1alpha1.GitRepoSpec{
@@ -54,7 +64,10 @@ var _ = Describe("GitRepo", Ordered, func() {
 			Expect(gitrepo.Spec.PollingInterval).To(BeNil())
 
 			Eventually(func() bool {
-				ns := types.NamespacedName{Name: "git-test-gitrepo", Namespace: namespace}
+				ns := types.NamespacedName{
+					Name:      fmt.Sprintf("git-%s", gitrepoName),
+					Namespace: namespace,
+				}
 
 				if err := k8sClient.Get(ctx, ns, &corev1.ServiceAccount{}); err != nil {
 					return false
@@ -73,7 +86,7 @@ var _ = Describe("GitRepo", Ordered, func() {
 		It("updates the gitrepo status", func() {
 			org := gitrepo.ResourceVersion
 			Eventually(func() bool {
-				_ = k8sClient.Get(ctx, types.NamespacedName{Name: "test-gitrepo", Namespace: namespace}, gitrepo)
+				_ = k8sClient.Get(ctx, types.NamespacedName{Name: gitrepoName, Namespace: namespace}, gitrepo)
 				return gitrepo.ResourceVersion > org
 			}).Should(BeTrue())
 
