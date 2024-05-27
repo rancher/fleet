16f27046d08336968991cb2b4a7bce099d693bc6
Rubhan Azeem <rubhanazeem@gmail.com>
2024-05-27 16:07:22 +0200

    Add status tests for bundledeployments

--- integrationtests/controller/bundledeployment/status_test.go
+++ integrationtests/controller/bundledeployment/status_test.go
@@ -5,9 +5,11 @@ import (
 	. "github.com/onsi/gomega"
 
 	"github.com/rancher/fleet/integrationtests/utils"
+	"github.com/rancher/fleet/pkg/apis/fleet.cattle.io/v1alpha1"
 
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 )
 
 var _ = Describe("BundleDeployment Status Fields", func() {
@@ -27,9 +29,69 @@ var _ = Describe("BundleDeployment Status Fields", func() {
 
 	When("BundleDeployment changes", func() {
 		BeforeEach(func() {
+			options := v1alpha1.BundleDeploymentOptions{
+				TargetNamespace: "targetNs",
+			}
+			bd, err := createBundleDeployment("name", namespace, options)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(bd).To(Not(BeNil()))
+		})
+
+		AfterEach(func() {
+			Expect(k8sClient.Delete(ctx, &v1alpha1.BundleDeployment{ObjectMeta: metav1.ObjectMeta{
+				Name:      "name",
+				Namespace: namespace,
+			}})).NotTo(HaveOccurred())
+
 		})
 
 		It("updates the status fields", func() {
+			bd := &v1alpha1.BundleDeployment{}
+			Eventually(func() error {
+				err := k8sClient.Get(ctx, types.NamespacedName{Namespace: namespace, Name: "name"}, bd)
+				if err != nil {
+					return err
+				}
+				bd.Status.AppliedDeploymentID = bd.Spec.DeploymentID
+				bd.Status.Ready = true
+				bd.Status.NonModified = true
+				return k8sClient.Status().Update(ctx, bd)
+			}).ShouldNot(HaveOccurred())
+
+			Eventually(func() error {
+				err := k8sClient.Get(ctx, types.NamespacedName{Namespace: namespace, Name: "name"}, bd)
+				Expect(err).ToNot(HaveOccurred())
+				bd.Spec.StagedDeploymentID = bd.Spec.DeploymentID
+				err = k8sClient.Update(ctx, bd)
+				return err
+			}).ShouldNot(HaveOccurred())
+			Expect(bd.Status.Display.State).To(Equal("Ready"))
+			Expect(bd.Spec.StagedDeploymentID).To(Equal(bd.Spec.DeploymentID))
+
+			By("Updating the bundledeployment spec, updates status fields")
+			Expect(bd.Spec.Options.DeleteNamespace).ToNot(BeTrue())
+
+			err := k8sClient.Get(ctx, types.NamespacedName{Namespace: namespace, Name: "name"}, bd)
+			Expect(err).NotTo(HaveOccurred())
+			bd.Status.NonModified = false
+			err = k8sClient.Status().Update(ctx, bd)
+			Expect(err).NotTo(HaveOccurred())
+
+			err = k8sClient.Get(ctx, types.NamespacedName{Namespace: namespace, Name: "name"}, bd)
+			Expect(bd.Status.NonModified).To(BeFalse())
+			Expect(bd.Status.Ready).To(BeTrue())
+			Expect(bd.Status.Display.State).To(Equal("Ready"))
+
+			Eventually(func() error {
+				err = k8sClient.Get(ctx, types.NamespacedName{Namespace: namespace, Name: "name"}, bd)
+				Expect(err).NotTo(HaveOccurred())
+				bd.Spec.Options.DeleteNamespace = true
+				return k8sClient.Update(ctx, bd)
+			}).ShouldNot(HaveOccurred())
+
+			err = k8sClient.Get(ctx, types.NamespacedName{Namespace: namespace, Name: "name"}, bd)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(bd.Status.Display.State).To(Equal("Modified"))
 		})
 	})
 })
--- integrationtests/controller/bundledeployment/suite_test.go
+++ integrationtests/controller/bundledeployment/suite_test.go
@@ -9,7 +9,9 @@ import (
 
 	"github.com/rancher/fleet/integrationtests/utils"
 	"github.com/rancher/fleet/internal/cmd/controller/reconciler"
+	"github.com/rancher/fleet/pkg/apis/fleet.cattle.io/v1alpha1"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/rest"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -66,3 +68,18 @@ var _ = AfterSuite(func() {
 	cancel()
 	Expect(testenv.Stop()).ToNot(HaveOccurred())
 })
+
+func createBundleDeployment(name, namespace string, options v1alpha1.BundleDeploymentOptions) (*v1alpha1.BundleDeployment, error) {
+	bundleDeployment := v1alpha1.BundleDeployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+			Labels:    map[string]string{"foo": "bar"},
+		},
+		Spec: v1alpha1.BundleDeploymentSpec{
+			Options: options,
+		},
+	}
+
+	return &bundleDeployment, k8sClient.Create(ctx, &bundleDeployment)
+}
