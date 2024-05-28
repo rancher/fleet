78c2174ff14b9cc67594a82d5221235dab9eef10
Rubhan Azeem <rubhanazeem@gmail.com>
2024-05-28 13:03:03 +0200

    Add status tests for clusterGroup

--- integrationtests/controller/clustergroup/status_test.go
+++ integrationtests/controller/clustergroup/status_test.go
@@ -5,12 +5,19 @@ import (
 	. "github.com/onsi/gomega"
 
 	"github.com/rancher/fleet/integrationtests/utils"
+	"github.com/rancher/fleet/pkg/apis/fleet.cattle.io/v1alpha1"
 
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 )
 
 var _ = Describe("ClusterGroup Status Fields", func() {
+
+	var (
+		clusterName = "test-cluster"
+		groupName   = "test-cluster-group"
+	)
 
 	BeforeEach(func() {
 		var err error
@@ -27,9 +34,35 @@ var _ = Describe("ClusterGroup Status Fields", func() {
 
 	When("Cluster is added", func() {
 		BeforeEach(func() {
+			clusterGroup, err := createClusterGroup(groupName, namespace, &metav1.LabelSelector{
+				MatchLabels: map[string]string{},
+			})
+			Expect(err).NotTo(HaveOccurred())
+			Expect(clusterGroup).To(Not(BeNil()))
 		})
 
 		It("updates the status fields", func() {
+			clusterGroup := &v1alpha1.ClusterGroup{}
+			err := k8sClient.Get(ctx, types.NamespacedName{Namespace: namespace, Name: groupName}, clusterGroup)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(clusterGroup).To(Not(BeNil()))
+			Expect(clusterGroup.Status.ClusterCount).To(Equal(0))
+			Expect(clusterGroup.Status.Display.ReadyClusters).To(Equal(""))
+
+			cluster, err := createCluster(clusterName, namespace, nil, namespace)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(cluster).To(Not(BeNil()))
+
+			Eventually(func() error {
+				err := k8sClient.Get(ctx, types.NamespacedName{Namespace: namespace, Name: groupName}, clusterGroup)
+				Expect(err).NotTo(HaveOccurred())
+				clusterGroup.Spec.Selector.MatchLabels = map[string]string{
+					"Name": clusterName,
+				}
+				return k8sClient.Update(ctx, clusterGroup)
+			}).ShouldNot(HaveOccurred())
+			Expect(clusterGroup.Status.ClusterCount).To(Equal(1))
+			Expect(clusterGroup.Status.Display.ReadyClusters).To(Equal("1/1"))
 		})
 	})
 })
--- integrationtests/controller/clustergroup/suite_test.go
+++ integrationtests/controller/clustergroup/suite_test.go
@@ -9,6 +9,8 @@ import (
 
 	"github.com/rancher/fleet/integrationtests/utils"
 	"github.com/rancher/fleet/internal/cmd/controller/reconciler"
+	"github.com/rancher/fleet/pkg/apis/fleet.cattle.io/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"k8s.io/client-go/rest"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -55,6 +57,14 @@ var _ = BeforeSuite(func() {
 	}).SetupWithManager(mgr)
 	Expect(err).ToNot(HaveOccurred(), "failed to set up manager")
 
+	err = (&reconciler.ClusterReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+
+		Query: &FakeQuery{},
+	}).SetupWithManager(mgr)
+	Expect(err).ToNot(HaveOccurred(), "failed to set up manager")
+
 	go func() {
 		defer GinkgoRecover()
 		err = mgr.Start(ctx)
@@ -66,3 +76,44 @@ var _ = AfterSuite(func() {
 	cancel()
 	Expect(testenv.Stop()).ToNot(HaveOccurred())
 })
+
+type FakeQuery struct {
+}
+
+// BundlesForCluster returns empty list, so no cleanup is needed
+func (q *FakeQuery) BundlesForCluster(context.Context, *v1alpha1.Cluster) ([]*v1alpha1.Bundle, []*v1alpha1.Bundle, error) {
+	return nil, nil, nil
+}
+
+func createCluster(name, controllerNs string, labels map[string]string, clusterNs string) (*v1alpha1.Cluster, error) {
+	cluster := &v1alpha1.Cluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: controllerNs,
+			Labels:    labels,
+		},
+	}
+	err := k8sClient.Create(ctx, cluster)
+	if err != nil {
+		return nil, err
+	}
+	// Need to set the status.Namespace as it is needed to create a BundleDeployment.
+	// Namespace is set by the Cluster controller. We need to do it manually because we are running just the Bundle controller.
+	cluster.Status.Namespace = clusterNs
+	err = k8sClient.Status().Update(ctx, cluster)
+	return cluster, err
+}
+
+func createClusterGroup(name, namespace string, selector *metav1.LabelSelector) (*v1alpha1.ClusterGroup, error) {
+	cg := &v1alpha1.ClusterGroup{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Spec: v1alpha1.ClusterGroupSpec{
+			Selector: selector,
+		},
+	}
+	err := k8sClient.Create(ctx, cg)
+	return cg, err
+}
