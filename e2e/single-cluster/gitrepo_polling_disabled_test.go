f7f4762e719b6ed11455f19c8dd10fc6b91da808
GIRARDI Tommy <tommy.girardi@suse.com>
2024-06-06 17:00:49 +0200

    label fix

--- e2e/single-cluster/gitrepo_polling_disabled_test.go
+++ e2e/single-cluster/gitrepo_polling_disabled_test.go
@@ -15,7 +15,7 @@ import (
 	"github.com/rancher/fleet/e2e/testenv/kubectl"
 )
 
-var _ = Describe("GitRepoPollingDisabled", func() {
+var _ = Describe("GitRepoPollingDisabled", Label("infra-setup"), func() {
 	var (
 		tmpDir           string
 		clonedir         string
