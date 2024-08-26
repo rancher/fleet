2870c676d99ce640c8a397c8d56f58f83877b28d
Corentin Néau <tan.neau@suse.com>
2024-08-26 08:28:10 +0200

    Remove leftover `disable-gitops` mentions (#2762)
    That flag was removed when merging the `GitRepo` and gitOps controllers.

--- internal/cmd/controller/gitops/operator.go
+++ internal/cmd/controller/gitops/operator.go
@@ -66,7 +66,6 @@ func App(zo *zap.Options) *cobra.Command {
 
 // HelpFunc hides the global flag from the help output
 func (c *GitOperator) HelpFunc(cmd *cobra.Command, strings []string) {
-	_ = cmd.Flags().MarkHidden("disable-gitops")
 	_ = cmd.Flags().MarkHidden("disable-metrics")
 	_ = cmd.Flags().MarkHidden("shard-id")
 	_ = cmd.Flags().MarkHidden("shard-node-selector")
