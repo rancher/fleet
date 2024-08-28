6da4914eabe7fe4039d425e50beb8d26601f1054
Corentin Néau <tan.neau@suse.com>
2024-08-28 16:35:23 +0200

    Fix downstream cluster registration
    When installing Rancher through Helm:
    * the `CATTLE_SERVER_URL` needs to be set to the same value as `hostname`
    * TLS mode must be set to `system-store`, to prevent cert-related errors
      when running a Fleet agent in a downstream cluster

--- .github/scripts/register-downstream-clusters.sh
+++ .github/scripts/register-downstream-clusters.sh
@@ -3,7 +3,7 @@
 set -euxo pipefail
 
 
-public_hostname="${public_hostname-172.18.0.1.sslip.io}"
+public_hostname="${public_hostname-172.28.0.2.sslip.io}"
 cluster_downstream="${cluster_downstream-k3d-downstream}"
 ctx=$(kubectl config current-context)
 
@@ -15,15 +15,9 @@ sed "s/user-zvnsr/$user/" <<'EOF' | kubectl apply -f -
 apiVersion: management.cattle.io/v3
 kind: Token
 authProvider: local
-current: false
-description: mytoken
-expired: false
-expiresAt: ""
-isDerived: true
-lastUpdateTime: ""
 metadata:
-  generateName: token-
   labels:
+    authn.management.cattle.io/kind: kubeconfig
     authn.management.cattle.io/token-userId: user-zvnsr
     cattle.io/creator: norman
   name: token-ci
@@ -41,6 +35,7 @@ userPrincipal:
   provider: local
 EOF
 
+# Log into the 4th project listed by `rancher login`, which should be the local cluster's default project.
 echo -e "4\n" | rancher login "https://$public_hostname" --token "$token" --skip-verify
 
 rancher clusters create second --import
@@ -49,6 +44,7 @@ id=$( rancher cluster ls --format json | jq -r 'select(.Name=="second") | .ID' )
 
 kubectl config use-context "$cluster_downstream"
 kubectl create clusterrolebinding cluster-admin-binding --clusterrole cluster-admin --user $user
+
 until [ -n "$(rancher cluster import "$id" | grep curl)" ]; do sleep 1; done
 rancher cluster import "$id" | grep curl | sh
 
@@ -58,3 +54,6 @@ until rancher cluster ls --format json | jq -r 'select(.Name=="second") | .Clust
 done
 
 kubectl config use-context "$ctx"
+
+# Wait for Fleet agent to be ready on downstream cluster
+until kubectl get bundles -n fleet-default | grep -q "fleet-agent-$id.*1/1"; do sleep 3; done
