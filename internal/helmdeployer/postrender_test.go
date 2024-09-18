77409957687ddf3e7c52d1541a0d624eabdf2aa5
Tim Hardeck <thardeck@suse.com>
2024-09-18 16:54:16 +0200

    Fix helm.sh/resource-policy being added to everything and not just CRDs (#2875)
    When `deleteCRDResources` was disabled (default), the
    `helm.sh/resource-policy` annotation was added to almost all resources
    instead of just to the CRDs as it was intended. The fix is to not change
    the shared `annotations` that also get added to other resources, but
    only modify a copy that's only used for one resource.
    
    Co-authored-by: Benjamin Neff <benjamin@coding4coffee.ch>

--- internal/helmdeployer/postrender_test.go
+++ internal/helmdeployer/postrender_test.go
@@ -116,4 +116,64 @@ func TestPostRenderer_Run_DeleteCRDs(t *testing.T) {
 			}
 		})
 	}
+
+	t.Run("Multiple resources, only add to CRDs", func(t *testing.T) {
+		crd := &apiextensionsv1.CustomResourceDefinition{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       CRDKind,
+				APIVersion: "apiextensions.k8s.io/v1",
+			},
+		}
+		pod := &corev1.Pod{
+			TypeMeta: metav1.TypeMeta{
+				Kind: "Pod",
+			},
+		}
+
+		data, err := yaml.ToBytes([]kruntime.Object{crd, pod})
+		if err != nil {
+			t.Errorf("unexpected error %v", err)
+		}
+		renderedManifests := bytes.NewBuffer(data)
+
+		pr := postRender{
+			manifest: &manifest.Manifest{
+				Resources: []v1alpha1.BundleResource{},
+			},
+			chart: &chart.Chart{},
+			opts: v1alpha1.BundleDeploymentOptions{
+				DeleteCRDResources: false,
+			},
+		}
+		postRenderedManifests, err := pr.Run(renderedManifests)
+		if err != nil {
+			t.Errorf("unexpected error %v", err)
+		}
+
+		data = postRenderedManifests.Bytes()
+		objs, err := yaml.ToObjects(bytes.NewBuffer(data))
+		if err != nil {
+			t.Errorf("unexpected error %v", err)
+		}
+
+		for _, obj := range objs {
+			m, err := meta.Accessor(obj)
+			if err != nil {
+				t.Errorf("unexpected error %v", err)
+			}
+
+			annotations := m.GetAnnotations()
+			kind := obj.GetObjectKind().GroupVersionKind().Kind
+			if kind == CRDKind {
+				if val, ok := annotations[kube.ResourcePolicyAnno]; !ok || val != kube.KeepPolicy {
+					t.Errorf("expected %s, got %s", kube.KeepPolicy, annotations[kube.ResourcePolicyAnno])
+				}
+			} else {
+				if val, ok := annotations[kube.ResourcePolicyAnno]; ok {
+					t.Errorf("unexpected annotation on %s, got %s: %s", kind, kube.ResourcePolicyAnno, val)
+				}
+			}
+		}
+	})
+
 }
