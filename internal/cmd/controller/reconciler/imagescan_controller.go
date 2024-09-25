61b4f761c98d154b6e5eb35ad00531a7baeae700
Mario Manno <mmanno@suse.com>
2024-09-25 13:10:56 +0200

    Increase worker count for reconcilers
    Increase other reconcilers worker count
    
    This should reflect the old settings from 0.9

--- internal/cmd/controller/reconciler/imagescan_controller.go
+++ internal/cmd/controller/reconciler/imagescan_controller.go
@@ -15,6 +15,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
 )
@@ -42,6 +43,7 @@ func (r *ImageScanReconciler) SetupWithManager(mgr ctrl.Manager) error {
 					predicate.LabelChangedPredicate{},
 				),
 			)).
+		WithOptions(controller.Options{MaxConcurrentReconciles: 50}).
 		Complete(r)
 }
 
