bff0a24f45f69e74e9903fd3626b7e993509f5fa
Mario Manno <mmanno@suse.com>
2024-09-25 13:10:56 +0200

    Increase worker count for reconcilers
    Increase other reconcilers worker count
    
    This should reflect the old settings from 0.9

--- internal/cmd/agent/controller/bundledeployment_controller.go
+++ internal/cmd/agent/controller/bundledeployment_controller.go
@@ -21,6 +21,7 @@ import (
 	"k8s.io/client-go/util/retry"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
@@ -78,6 +79,7 @@ func (r *BundleDeploymentReconciler) SetupWithManager(mgr ctrl.Manager) error {
 					},
 				},
 			)).
+		WithOptions(controller.Options{MaxConcurrentReconciles: 50}).
 		Complete(r)
 }
 
--- internal/cmd/controller/reconciler/cluster_controller.go
+++ internal/cmd/controller/reconciler/cluster_controller.go
@@ -29,6 +29,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/log"
@@ -100,6 +101,7 @@ func (r *ClusterReconciler) SetupWithManager(mgr ctrl.Manager) error {
 			}),
 		).
 		WithEventFilter(sharding.FilterByShardID(r.ShardID)).
+		WithOptions(controller.Options{MaxConcurrentReconciles: 50}).
 		Complete(r)
 }
 
