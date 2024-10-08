40133de4e71f25700e12052f566a687c793c762a
Patrick Seidensal <pseidensal@suse.com>
2024-10-08 09:59:53 +0200

    Wait for ingress to appear before waiting for load balancer
    Prevents
    
    ```
    Error from server (NotFound): the server could not find the requested resource
    ```
    
    while deploying.

--- .github/scripts/wait-for-loadbalancer.sh
+++ .github/scripts/wait-for-loadbalancer.sh
@@ -2,6 +2,11 @@
 
 set -euxo pipefail
 
+# wait for Rancher to create the ingress before waiting for the loadbalancer
+while ! kubectl get ingress -n cattle-system rancher; do
+  sleep 1
+done
+
 # wait for loadBalancer IPs
 { grep -q -m 1 -e ".*"; kill $!; } < <(kubectl get ingress -n cattle-system rancher -o 'go-template={{range .status.loadBalancer.ingress}}{{.ip}}{{"\n"}}{{end}}' -w)
 # wait for certificate
