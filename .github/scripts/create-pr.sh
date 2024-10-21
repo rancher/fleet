e2818ea02d91c12bdb609d03c30100891d9337ac
Tim Hardeck <thardeck@suse.com>
2024-10-21 18:49:48 +0200

    Replace peter-evans/create-pull-request GHA by usign gh client

---
+++ .github/scripts/create-pr.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+target_branch=$1
+new_fleet=$2
+new_chart=$3
+repo=$4
+
+# Check if the environment variable is set
+if [ -z "$GITHUB_TOKEN" ]; then
+    echo "Environment variable GITHUB_TOKEN is not set."
+    exit 1
+fi
+
+# Configure git login
+git config --local --unset http.https://github.com/.extraheader ^AUTHORIZATION:
+gh auth setup-git
+
+# Create and push new branch
+git remote add fork "https://github.com/rancherbot/$repo"
+BRANCH_NAME="fleet-$(date +%s)"
+git checkout -b "$BRANCH_NAME"
+git push fork "$BRANCH_NAME"
+
+# Create a pull request
+gh pr create --title "[${target_branch}] fleet ${new_chart}+up${new_fleet} update" \
+             --body "Update Fleet to v${new_fleet}\n\nChangelog: https://github.com/rancher/fleet/releases/tag/v${new_fleet}" \
+             --base "${target_branch}" \
+             --repo "rancher/$repo" --head "rancherbot:$BRANCH_NAME"
