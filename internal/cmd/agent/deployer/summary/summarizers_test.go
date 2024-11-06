5a355bada19b4c8c2f4ebeeef7b140d23a030189
Patrick Seidensal <pseidensal@suse.com>
2024-11-06 14:44:59 +0100

    Extend test cases

--- internal/cmd/agent/deployer/summary/summarizers_test.go
+++ internal/cmd/agent/deployer/summary/summarizers_test.go
@@ -10,22 +10,47 @@ import (
 
 var _ = Describe("Summary", func() {
 	When("testing kStatusSummarizer", func() {
-		It("should deduplicate messages", func() {
-			obj := data.Object{
+		newObj := func(message string) data.Object {
+			return data.Object{
 				"status": map[string]interface{}{
 					"conditions": []interface{}{
 						map[string]interface{}{
 							"type":    "Ready",
 							"status":  "False",
-							"message": "foo; bar; foo; foo; bar",
+							"message": message,
 						},
 					},
 				},
 			}
+		}
+
+		It("should deduplicate messages", func() {
+			obj := newObj("message1; message2; message1; message1; message2")
 			summary := kStatusSummarizer(obj, nil, fleetv1.Summary{})
-			Expect(summary.Message).To(Equal([]string{"foo; bar"}))
+			Expect(summary.Message).To(Equal([]string{"message1; message2"}))
 			Expect(summary.Transitioning).To(BeTrue())
 			Expect(summary.Error).To(BeFalse())
+		})
+
+		It("should not alter the message that doesn't contain duplicates", func() {
+			obj := newObj("message1; message2; message3")
+			summary := kStatusSummarizer(obj, nil, fleetv1.Summary{})
+			Expect(summary.Message).To(Equal([]string{"message1; message2; message3"}))
+		})
+
+		It("should not deduplicate messages with the wrong separator", func() {
+			separators := []string{", ", " ", " , ", "[", "]", "{", "}"}
+			for _, sep := range separators {
+				obj := newObj("message1" + sep + "message1")
+				summary := kStatusSummarizer(obj, nil, fleetv1.Summary{})
+				Expect(summary.Message).To(Equal([]string{"message1" + sep + "message1"}))
+			}
+		})
+
+		It("should not fail on an empty message", func() {
+			obj := newObj("")
+			summary := kStatusSummarizer(obj, nil, fleetv1.Summary{})
+			Expect(summary.Message).To(BeEmpty())
 		})
 	})
 })
