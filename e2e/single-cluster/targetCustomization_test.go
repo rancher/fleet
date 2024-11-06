15177f9dab016365580634107b198fb4d39d49ee
Corentin Néau <tan.neau@suse.com>
2024-11-06 12:27:18 +0100

    Improve namespace target customization tests
    These tests now verify that the created namespace does bear expected
    labels and annotations.
    This commit also paves the way for additional tests with customizations
    over unconfigured namespace labels and annotations, which currently
    cause a panic.

--- e2e/single-cluster/targetCustomization_test.go
+++ e2e/single-cluster/targetCustomization_test.go
@@ -10,7 +10,7 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Helm deploy options", func() {
+var _ = FDescribe("Helm deploy options", func() {
 	var (
 		asset string
 		k     kubectl.Command
@@ -30,70 +30,69 @@ var _ = Describe("Helm deploy options", func() {
 	})
 
 	Describe("namespaceLabels TargetCustomization", func() {
+		// TODO add a test case without defaults
 		BeforeEach(func() {
-			asset = "single-cluster/namespaceLabels_targetCustomization.yaml"
+			asset = "single-cluster/ns-labels-target-customization.yaml"
 		})
 		When("namespaceLabels and namespaceAnnotations are set as targetCustomization ", func() {
 			It("deploys the bundledeployment with the merged namespaceLabels and namespaceAnnotations", func() {
+				By("setting the namespaces and annotations on the bundle deployment")
 				out, err := k.Get("cluster", "local", "-o", "jsonpath={.status.namespace}")
 				Expect(err).ToNot(HaveOccurred(), out)
 
 				clusterNS := strings.TrimSpace(out)
 				clusterK := k.Namespace(clusterNS)
-				Eventually(func() string {
+
+				var bundleDeploymentName string
+
+				Eventually(func(g Gomega) {
 					bundleDeploymentNames, _ := clusterK.Get(
 						"bundledeployments",
 						"-o",
 						"jsonpath={.items[*].metadata.name}",
 					)
 
-					var bundleDeploymentName string
-					for _, podName := range strings.Split(bundleDeploymentNames, " ") {
-						if strings.HasPrefix(podName, "test-target-customization-namespace-labels") {
-							bundleDeploymentName = podName
+					for _, bdName := range strings.Split(bundleDeploymentNames, " ") {
+						if strings.HasPrefix(bdName, "test-target-customization-namespace-labels") {
+							bundleDeploymentName = bdName
 							break
 						}
 					}
-					if bundleDeploymentName == "" {
-						return "nil"
-					}
 
-					bundleDeploymentNamespacesLabels, _ := clusterK.Get(
+					g.Expect(bundleDeploymentName).ToNot(BeEmpty())
+				}).Should(Succeed())
+
+				Eventually(func(g Gomega) {
+					labels, err := clusterK.Get(
 						"bundledeployments",
 						bundleDeploymentName,
 						"-o",
 						"jsonpath={.spec.options.namespaceLabels}",
 					)
-					return bundleDeploymentNamespacesLabels
-				}).Should(Equal(`{"foo":"bar","this.is/a":"test"}`))
 
-				Eventually(func() string {
-					bundleDeploymentNames, _ := clusterK.Get(
-						"bundledeployments",
-						"-o",
-						"jsonpath={.items[*].metadata.name}",
-					)
+					g.Expect(err).ToNot(HaveOccurred())
+					g.Expect(labels).To(Equal(`{"foo":"bar","this.is/a":"test"}`))
 
-					var bundleDeploymentName string
-					for _, podName := range strings.Split(bundleDeploymentNames, " ") {
-						if strings.HasPrefix(podName, "test-target-customization-namespace-labels") {
-							bundleDeploymentName = podName
-							break
-						}
-					}
-					if bundleDeploymentName == "" {
-						return "nil"
-					}
-
-					bundleDeploymentNamespacesLabels, _ := clusterK.Get(
+					annot, err := clusterK.Get(
 						"bundledeployments",
 						bundleDeploymentName,
 						"-o",
 						"jsonpath={.spec.options.namespaceAnnotations}",
 					)
-					return bundleDeploymentNamespacesLabels
-				}).Should(Equal(`{"foo":"bar","this.is/a":"test"}`))
+					g.Expect(err).ToNot(HaveOccurred())
+					g.Expect(annot).To(Equal(`{"foo":"bar","this.is/another":"test"}`))
+				}).Should(Succeed())
 
+				By("setting the namespaces and annotations on the created namespace")
+				Eventually(func(g Gomega) {
+					labels, err := k.Get("ns", "ns-1", "-o", "jsonpath={.metadata.labels}")
+					g.Expect(err).ToNot(HaveOccurred())
+					g.Expect(labels).To(Equal(`{"foo":"bar","kubernetes.io/metadata.name":"ns-1","this.is/a":"test"}`))
+
+					ann, err := k.Get("ns", "ns-1", "-o", "jsonpath={.metadata.annotations}")
+					g.Expect(err).ToNot(HaveOccurred())
+					g.Expect(ann).To(Equal(`{"foo":"bar","this.is/another":"test"}`))
+				}).Should(Succeed())
 			})
 		})
 	})
