34170715e2947a413c0d51fae3da6c525f2c0257
Patrick Seidensal <9606095+p-se@users.noreply.github.com>
2025-01-09 10:40:57 +0100

    [SURE-9137] Add template errors to bundle and gitrepo status (#3114)
    * Import v1alpha1 package as fleet
    
    * Show bundle errors in Bundle and GitRepo
    
    Refers to #2943
    
    * Add E2E tests
    
    Refers to #2943
    
    (cherry picked from commit 235e8efa1e857fc482ddafc7b415ec760b343d24)

--- internal/cmd/controller/gitops/reconciler/status_controller.go
+++ internal/cmd/controller/gitops/reconciler/status_controller.go
@@ -8,10 +8,12 @@ import (
 
 	"github.com/rancher/fleet/internal/cmd/controller/summary"
 	"github.com/rancher/fleet/internal/resourcestatus"
-	v1alpha1 "github.com/rancher/fleet/pkg/apis/fleet.cattle.io/v1alpha1"
+	fleet "github.com/rancher/fleet/pkg/apis/fleet.cattle.io/v1alpha1"
 	"github.com/rancher/fleet/pkg/durations"
 	"github.com/rancher/fleet/pkg/sharding"
+	"github.com/rancher/wrangler/v3/pkg/genericcondition"
 
+	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
@@ -34,12 +36,12 @@ type StatusReconciler struct {
 
 func (r *StatusReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&v1alpha1.GitRepo{}).
+		For(&fleet.GitRepo{}).
 		Watches(
 			// Fan out from bundle to gitrepo
-			&v1alpha1.Bundle{},
+			&fleet.Bundle{},
 			handler.EnqueueRequestsFromMapFunc(func(ctx context.Context, a client.Object) []ctrl.Request {
-				repo := a.GetLabels()[v1alpha1.RepoLabel]
+				repo := a.GetLabels()[fleet.RepoLabel]
 				if repo != "" {
 					return []ctrl.Request{{
 						NamespacedName: types.NamespacedName{
@@ -64,7 +66,7 @@ func (r *StatusReconciler) SetupWithManager(mgr ctrl.Manager) error {
 // display information to the user.
 func (r *StatusReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	logger := log.FromContext(ctx).WithName("gitops-status")
-	gitrepo := &v1alpha1.GitRepo{}
+	gitrepo := &fleet.GitRepo{}
 
 	if err := r.Get(ctx, req.NamespacedName, gitrepo); err != nil && !errors.IsNotFound(err) {
 		return ctrl.Result{}, err
@@ -93,10 +95,10 @@ func (r *StatusReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctr
 
 	logger.V(1).Info("Reconciling GitRepo status")
 
-	bdList := &v1alpha1.BundleDeploymentList{}
+	bdList := &fleet.BundleDeploymentList{}
 	err := r.List(ctx, bdList, client.MatchingLabels{
-		v1alpha1.RepoLabel:            gitrepo.Name,
-		v1alpha1.BundleNamespaceLabel: gitrepo.Namespace,
+		fleet.RepoLabel:            gitrepo.Name,
+		fleet.BundleNamespaceLabel: gitrepo.Namespace,
 	})
 	if err != nil {
 		return ctrl.Result{}, err
@@ -109,6 +111,29 @@ func (r *StatusReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctr
 
 	if gitrepo.Status.GitJobStatus != "Current" {
 		gitrepo.Status.Display.State = "GitUpdating"
+	}
+
+	// We're explicitly setting the ready status from a bundle here, but only if it isn't ready.
+	//
+	// - If the bundle has no deployments, there is no status to be copied from the setStatus
+	// function, so that we won't overwrite anything.
+	//
+	// - If the bundle has rendering issues and there are deployments of which there is at least one
+	// in a failed state, the status of the bundle deployments would be overwritten by the bundle
+	// status.
+	//
+	// - If the bundle has no rendering issues but there are deployments in a failed state, the code
+	// will overwrite the gitrepo's ready status condition with the ready status condition coming
+	// from the bundle. Because both have the same content, we can unconditionally set the status
+	// from the bundle.
+	//
+	// So we're basically just making sure the status from the bundle is being set on the gitrepo,
+	// even if there are no bundle deployments, which is the case for issues with rendering the
+	// manifests, for instance. In that case no bundle deployments are created, but an error is set
+	// in a ready status condition on the bundle.
+	err = r.setReadyStatusFromBundle(ctx, gitrepo)
+	if err != nil {
+		return ctrl.Result{}, err
 	}
 
 	err = r.Client.Status().Update(ctx, gitrepo)
@@ -128,11 +153,11 @@ func bundleStatusChangedPredicate() predicate.Funcs {
 			return true
 		},
 		UpdateFunc: func(e event.UpdateEvent) bool {
-			n, isBundle := e.ObjectNew.(*v1alpha1.Bundle)
+			n, isBundle := e.ObjectNew.(*fleet.Bundle)
 			if !isBundle {
 				return false
 			}
-			o := e.ObjectOld.(*v1alpha1.Bundle)
+			o := e.ObjectOld.(*fleet.Bundle)
 			if n == nil || o == nil {
 				return false
 			}
@@ -144,7 +169,7 @@ func bundleStatusChangedPredicate() predicate.Funcs {
 	}
 }
 
-func setStatus(list *v1alpha1.BundleDeploymentList, gitrepo *v1alpha1.GitRepo) error {
+func setStatus(list *fleet.BundleDeploymentList, gitrepo *fleet.GitRepo) error {
 	// sort for resourceKey?
 	sort.Slice(list.Items, func(i, j int) bool {
 		return list.Items[i].UID < list.Items[j].UID
@@ -168,21 +193,21 @@ func setStatus(list *v1alpha1.BundleDeploymentList, gitrepo *v1alpha1.GitRepo) e
 
 // setFields sets bundledeployment related status fields:
 // Summary, ReadyClusters, DesiredReadyClusters, Display.State, Display.Message, Display.Error
-func setFields(list *v1alpha1.BundleDeploymentList, gitrepo *v1alpha1.GitRepo) error {
+func setFields(list *fleet.BundleDeploymentList, gitrepo *fleet.GitRepo) error {
 	var (
-		maxState   v1alpha1.BundleState
+		maxState   fleet.BundleState
 		message    string
 		count      = map[client.ObjectKey]int{}
 		readyCount = map[client.ObjectKey]int{}
 	)
 
-	gitrepo.Status.Summary = v1alpha1.BundleSummary{}
+	gitrepo.Status.Summary = fleet.BundleSummary{}
 
 	for _, bd := range list.Items {
 		state := summary.GetDeploymentState(&bd)
 		summary.IncrementState(&gitrepo.Status.Summary, bd.Name, state, summary.MessageFromDeployment(&bd), bd.Status.ModifiedStatus, bd.Status.NonReadyStatus)
 		gitrepo.Status.Summary.DesiredReady++
-		if v1alpha1.StateRank[state] > v1alpha1.StateRank[maxState] {
+		if fleet.StateRank[state] > fleet.StateRank[maxState] {
 			maxState = state
 			message = summary.MessageFromDeployment(&bd)
 		}
@@ -194,8 +219,8 @@ func setFields(list *v1alpha1.BundleDeploymentList, gitrepo *v1alpha1.GitRepo) e
 			continue
 		}
 
-		name := bd.Labels[v1alpha1.ClusterLabel]
-		namespace := bd.Labels[v1alpha1.ClusterNamespaceLabel]
+		name := bd.Labels[fleet.ClusterLabel]
+		namespace := bd.Labels[fleet.ClusterNamespaceLabel]
 		if name == "" || namespace == "" {
 			// this should not happen
 			continue
@@ -203,7 +228,7 @@ func setFields(list *v1alpha1.BundleDeploymentList, gitrepo *v1alpha1.GitRepo) e
 
 		key := client.ObjectKey{Name: name, Namespace: namespace}
 		count[key]++
-		if state == v1alpha1.Ready {
+		if state == fleet.Ready {
 			readyCount[key]++
 		}
 	}
@@ -220,7 +245,7 @@ func setFields(list *v1alpha1.BundleDeploymentList, gitrepo *v1alpha1.GitRepo) e
 	}
 	gitrepo.Status.ReadyClusters = readyClusters
 
-	if maxState == v1alpha1.Ready {
+	if maxState == fleet.Ready {
 		maxState = ""
 		message = ""
 	}
@@ -228,6 +253,63 @@ func setFields(list *v1alpha1.BundleDeploymentList, gitrepo *v1alpha1.GitRepo) e
 	gitrepo.Status.Display.State = string(maxState)
 	gitrepo.Status.Display.Message = message
 	gitrepo.Status.Display.Error = len(message) > 0
+
+	return nil
+}
+
+// setReadyStatusFromBundle fetches all bundles from a given gitrepo, checks the ready status conditions
+// from the bundles and applies one on the gitrepo if it isn't ready. The purpose is to make
+// rendering issues visible in the gitrepo status. Those issues need to be made explicitly visible
+// since the other statuses are calculated from bundle deployments, which do not exist when
+// rendering manifests fail. Should an issue be on the bundle, it will be copied to the gitrepo.
+func (r StatusReconciler) setReadyStatusFromBundle(ctx context.Context, gitrepo *fleet.GitRepo) error {
+	bList := &fleet.BundleList{}
+	err := r.List(ctx, bList, client.MatchingLabels{
+		fleet.RepoLabel: gitrepo.Name,
+	}, client.InNamespace(gitrepo.Namespace))
+	if err != nil {
+		return err
+	}
+
+	found := false
+	// Find a ready status condition in a bundle which is not ready.
+	var condition genericcondition.GenericCondition
+bundles:
+	for _, bundle := range bList.Items {
+		if bundle.Status.Conditions == nil {
+			continue
+		}
+
+		for _, c := range bundle.Status.Conditions {
+			if c.Type == string(fleet.Ready) && c.Status == v1.ConditionFalse {
+				condition = c
+				found = true
+				break bundles
+			}
+		}
+	}
+
+	// No ready condition found in any bundle, nothing to do here.
+	if !found {
+		return nil
+	}
+
+	found = false
+	newConditions := make([]genericcondition.GenericCondition, 0, len(gitrepo.Status.Conditions))
+	for _, c := range gitrepo.Status.Conditions {
+		if c.Type == string(fleet.Ready) {
+			// Replace the ready condition with the one from the bundle
+			newConditions = append(newConditions, condition)
+			found = true
+			continue
+		}
+		newConditions = append(newConditions, c)
+	}
+	if !found {
+		// Add the ready condition from the bundle to the gitrepo.
+		newConditions = append(newConditions, condition)
+	}
+	gitrepo.Status.Conditions = newConditions
 
 	return nil
 }
