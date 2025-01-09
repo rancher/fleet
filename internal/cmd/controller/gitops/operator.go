db9c50a1ab838a02192c5932bc2a345565a02d85
Mario Manno <mmanno@suse.com>
2025-01-09 15:04:37 +0100

    Gitops controller uses shorter resync interval
    If gitrepos are lost from the requeueAfter polling, resync should add them
    again.

--- internal/cmd/controller/gitops/operator.go
+++ internal/cmd/controller/gitops/operator.go
@@ -33,9 +33,10 @@ import (
 )
 
 var (
-	scheme   = runtime.NewScheme()
-	setupLog = ctrl.Log.WithName("setup")
-	zopts    *zap.Options
+	scheme            = runtime.NewScheme()
+	setupLog          = ctrl.Log.WithName("setup")
+	zopts             *zap.Options
+	defaultSyncPeriod = 10 * time.Hour
 )
 
 func init() {
@@ -94,6 +95,14 @@ func (g *GitOperator) Run(cmd *cobra.Command, args []string) error {
 		shardIDSuffix = fmt.Sprintf("-%s", g.ShardID)
 	}
 
+	syncPeriod := defaultSyncPeriod
+	if d := os.Getenv("GITREPO_SYNC_PERIOD"); d != "" {
+		syncPeriod, err = time.ParseDuration(d)
+		if err != nil {
+			return err
+		}
+	}
+
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
 		Scheme:                  scheme,
 		Metrics:                 g.setupMetrics(),
@@ -103,6 +112,10 @@ func (g *GitOperator) Run(cmd *cobra.Command, args []string) error {
 		LeaseDuration:           leaderOpts.LeaseDuration,
 		RenewDeadline:           leaderOpts.RenewDeadline,
 		RetryPeriod:             leaderOpts.RetryPeriod,
+		// resync to pick up lost gitrepos
+		Cache: cache.Options{
+			SyncPeriod: &syncPeriod,
+		},
 	})
 
 	if err != nil {
