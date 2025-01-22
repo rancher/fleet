85c81574383b8c9b3eabf992314da2898130cbb9
Corentin Néau <tan.neau@suse.com>
2025-01-22 18:32:43 +0100

    Migrate testing for volumes and args to unit tests
    That logic was already close to covered in unit tests. This moves it
    there, to remove clutter in integration tests, at the risk of adding
    some to unit tests.

--- integrationtests/gitjob/controller/controller_test.go
+++ integrationtests/gitjob/controller/controller_test.go
@@ -260,43 +260,6 @@ var _ = Describe("GitJob controller", func() {
 					g.Expect(secret.ObjectMeta).To(beOwnedBy(gitRepoOwnerRef))
 				}, time.Second*5, time.Second*1).Should(Succeed())
 
-				By("feeding the mounted CA bundle to the fleet apply command for the git cloner")
-				Eventually(func(g Gomega) {
-					err := k8sClient.Get(ctx, types.NamespacedName{Name: jobName, Namespace: gitRepoNamespace}, &job)
-					// Ignore not-found errors in case the job has not yet been created
-					g.Expect(client.IgnoreNotFound(err)).ToNot(HaveOccurred())
-				})
-
-				volumes := job.Spec.Template.Spec.Volumes
-				Expect(volumes).ToNot(BeEmpty())
-
-				found := false
-				for _, v := range volumes {
-					if v.Name == "additional-ca" {
-						found = true
-						break
-					}
-				}
-				Expect(found).To(BeTrue())
-
-				initContainers := job.Spec.Template.Spec.InitContainers
-				Expect(initContainers).ToNot(BeEmpty())
-				Expect(strings.Join(initContainers[0].Args, " ")).
-					To(ContainSubstring("--ca-bundle-file /gitjob/cabundle/additional-ca.crt"))
-
-				volumeMounts := initContainers[0].VolumeMounts
-				Expect(volumeMounts).ToNot(BeEmpty())
-
-				found = false
-				for _, vm := range volumeMounts {
-					if vm.Name == "additional-ca" {
-						found = true
-						Expect(vm.MountPath).To(Equal("/gitjob/cabundle"))
-						break
-					}
-				}
-				Expect(found).To(BeTrue())
-
 				By("creating a CA bundle secret for the Helm client")
 				secretName = fmt.Sprintf("%s-rancher-cabundle", gitRepoName)
 				ns = types.NamespacedName{Name: secretName, Namespace: gitRepo.Namespace}
@@ -310,30 +273,6 @@ var _ = Describe("GitJob controller", func() {
 					g.Expect(data).To(Equal([]byte("foo")))
 					g.Expect(secret.ObjectMeta).To(beOwnedBy(gitRepoOwnerRef))
 				}, time.Second*5, time.Second*1).Should(Succeed())
-
-				By("feeding the mounted CA bundle to the fleet apply command for Helm")
-				found = false
-				for _, v := range volumes {
-					if v.Name == "rancher-helm-secret-cert" {
-						found = true
-						break
-					}
-				}
-
-				Expect(found).To(BeTrue())
-				containers := job.Spec.Template.Spec.Containers
-				volumeMounts = containers[0].VolumeMounts
-				Expect(strings.Join(containers[0].Args, " ")).To(ContainSubstring("--cacerts-file /etc/rancher/certs/cacerts"))
-
-				found = false
-				for _, vm := range volumeMounts {
-					if vm.Name == "rancher-helm-secret-cert" {
-						found = true
-						Expect(vm.MountPath).To(Equal("/etc/ssl/certs"))
-						break
-					}
-				}
-				Expect(found).To(BeTrue())
 			})
 		})
 
