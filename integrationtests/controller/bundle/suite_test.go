920827d5be763ff32fac9977696133355918c4a2
Mario Manno <mmanno@suse.com>
2025-02-06 18:40:44 +0100

    Hide controller logs in integrationtests
    This introduces new environment variables for integrationtests:
    
    * CI_SILENCE_CTRL - no logging output, even when test fails
    * CI_KUBECONFIG - write kubeconfig for testenv to this path
    * CI_USE_EXISTING_CLUSTER - integrationtests don't setup testenv, but
      use existing cluster from current KUBECONFIG

--- integrationtests/controller/bundle/suite_test.go
+++ integrationtests/controller/bundle/suite_test.go
@@ -19,19 +19,16 @@ import (
 
 	"k8s.io/client-go/rest"
 
-	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
-	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 )
 
 var (
-	cancel      context.CancelFunc
-	cfg         *rest.Config
-	ctx         context.Context
-	k8sClient   client.Client
-	testenv     *envtest.Environment
-	testEnvBool bool
+	cancel    context.CancelFunc
+	cfg       *rest.Config
+	ctx       context.Context
+	k8sClient client.Client
+	testenv   *envtest.Environment
 
 	namespace string
 )
@@ -46,17 +43,14 @@ var _ = BeforeSuite(func() {
 	SetDefaultEventuallyPollingInterval(1 * time.Second)
 
 	ctx, cancel = context.WithCancel(context.TODO())
-	testenv = utils.NewEnvTest()
-	testEnvBool = true
+	testenv = utils.NewEnvTest("../../..")
 
 	var err error
-	cfg, err = testenv.Start()
+	cfg, err = utils.StartTestEnv(testenv)
 	Expect(err).NotTo(HaveOccurred())
 
 	k8sClient, err = utils.NewClient(cfg)
 	Expect(err).NotTo(HaveOccurred())
-
-	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&zap.Options{Development: true})))
 
 	mgr, err := utils.NewManager(cfg)
 	Expect(err).ToNot(HaveOccurred())
