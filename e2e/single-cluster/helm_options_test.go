a3bb0a04a5f79b365dbd1116a715f80f2c4cd187
Corentin Néau <tan.neau@suse.com>
2025-03-11 16:39:29 +0100

    Reduce setup loops in end-to-end tests
    This uses `By` statements to distinguish use cases with common setup,
    instead of having each of them live in its own `It` block, which would
    lead to that setup logic being run again.

--- e2e/single-cluster/helm_options_test.go
+++ e2e/single-cluster/helm_options_test.go
@@ -31,18 +31,17 @@ var _ = Describe("Helm deploy options", func() {
 		BeforeEach(func() {
 			asset = "single-cluster/helm-options-disabledns.yaml"
 		})
-		When("is false", func() {
-			bundleName := "helm-options-disabledns-helm-disable-dns-not-set"
-			It("enables DNS when invoking helm", func() {
+		When("toggling DisableDNS", func() {
+			It("honors DisableDNS", func() {
+				By("enabling DNS when invoking helm if DisableDNS is false")
+				bundleName := "helm-options-disabledns-helm-disable-dns-not-set"
 				Eventually(func() string {
 					out, _ := k.Get("bundle", bundleName, `-o=jsonpath={.status.conditions[?(@.type=="Ready")].status}`)
 					return out
 				}).Should(Equal("True"))
-			})
-		})
-		When("is true", func() {
-			bundleName := "helm-options-disabledns-helm-disable-dns-set"
-			It("does not enable DNS when invoking helm", func() {
+
+				By("not enabling DNS when invoking helm if DisableDNS is true")
+				bundleName = "helm-options-disabledns-helm-disable-dns-set"
 				Eventually(func() string {
 					out, _ := k.Get("bundle", bundleName, `-o=jsonpath='{.status.conditions[?(@.type=="Ready")].message}'`)
 					return out
@@ -55,18 +54,17 @@ var _ = Describe("Helm deploy options", func() {
 		BeforeEach(func() {
 			asset = "single-cluster/helm-options-skip-schema-validation.yaml"
 		})
-		When("is false", func() {
-			bundleName := "helm-options-skip-schema-val-helm-schemas-not-set"
-			It("fails when schema validation does not pass", func() {
+		When("toggling SkipSchemaValidation", func() {
+			It("honors SkipSchemaValidation", func() {
+				By("enabling SkipSchemaValidation and failing when schema validation does not pass")
+				bundleName := "helm-options-skip-schema-val-helm-schemas-not-set"
 				Eventually(func() string {
 					out, _ := k.Get("bundle", bundleName, `-o=jsonpath='{.status.conditions[?(@.type=="Ready")].message}'`)
 					return out
 				}).Should(ContainSubstring("values don't meet the specifications of the schema"))
-			})
-		})
-		When("is true", func() {
-			bundleName := "helm-options-skip-schema-val-helm-schemas-set"
-			It("completely skips the schema validation", func() {
+
+				By("completely skipping schema validation when disabled")
+				bundleName = "helm-options-skip-schema-val-helm-schemas-set"
 				Eventually(func() string {
 					out, _ := k.Get("bundle", bundleName, `-o=jsonpath={.status.conditions[?(@.type=="Ready")].status}`)
 					return out
