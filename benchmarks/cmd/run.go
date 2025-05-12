7efd35d31cd7ec915096769e5786262c3857f4e5
Mario Manno <mario.manno@suse.com>
2025-05-12 15:57:21 +0200

    Pass the benchmark env var to the dev script

--- benchmarks/cmd/run.go
+++ benchmarks/cmd/run.go
@@ -16,41 +16,9 @@ import (
 	"github.com/rancher/fleet/e2e/testenv/kubectl"
 )
 
-/*
-#!/bin/bash
-set -e
-
-date=$(date +"%F_%T")
-out="b-$date.json"
-FLEET_BENCH_OUTPUT=${FLEET_BENCH_OUTPUT-$out}
-FLEET_BENCH_TIMEOUT=${FLEET_BENCH_TIMEOUT-"5m"}
-FLEET_BENCH_NAMESPACE=${FLEET_BENCH_NAMESPACE-"fleet-local"}
-FLEET_BENCH_METRICS=${FLEET_BENCH_METRICS-"true"}
-
-export FLEET_BENCH_TIMEOUT
-export FLEET_BENCH_NAMESPACE
-export FLEET_BENCH_METRICS
-
-n=$(kubectl get clusters.fleet.cattle.io -n "$FLEET_BENCH_NAMESPACE" -l fleet.cattle.io/benchmark=true  -ojson | jq '.items | length')
-if [ "$n" -eq 0 ]; then
-
-	echo "No clusters found to benchmark"
-	echo "You need at least one cluster with the label fleet.cattle.io/benchmark=true"
-	echo
-	echo "Example:"
-	echo "kubectl label clusters.fleet.cattle.io -n fleet-local local fleet.cattle.io/benchmark=true"
-	exit 1
-
-fi
-
-ginkgo run --fail-fast --seed 1731598958 --json-report "$FLEET_BENCH_OUTPUT" ./benchmarks
-k = kubectl.New("", workspace)
-
-go run ./benchmarks/cmd report -d benchmarks/db -i "$FLEET_BENCH_OUTPUT"
-*/
-
-// runCmd runs the ginkgo tests, it is configured via env variables.
+// runCmd runs the ginkgo tests, it is configured via command line arguments
 //
+// It will set these environment variables:
 // FLEET_BENCH_OUTPUT=${$out}
 // FLEET_BENCH_TIMEOUT=${"5m"}
 // FLEET_BENCH_NAMESPACE=${"fleet-local"}
--- dev/benchmarks.sh
+++ dev/benchmarks.sh
@@ -5,10 +5,5 @@ date=$(date +"%F_%T")
 out="b-$date.json"
 FLEET_BENCH_TIMEOUT=${FLEET_BENCH_TIMEOUT-"5m"}
 FLEET_BENCH_NAMESPACE=${FLEET_BENCH_NAMESPACE-"fleet-local"}
-FLEET_BENCH_METRICS=${FLEET_BENCH_METRICS-"true"}
 
-export FLEET_BENCH_TIMEOUT
-export FLEET_BENCH_NAMESPACE
-export FLEET_BENCH_METRICS
-
-go run ./benchmarks/cmd run -d benchmarks/db
+go run ./benchmarks/cmd run -d benchmarks/db -t "$FLEET_BENCH_TIMEOUT" -n "$FLEET_BENCH_NAMESPACE"
