70d3f4e49ffa1788d4d3e8587039219ce2a64def
Tim Hardeck <thardeck@suse.com>
2025-05-12 17:37:50 +0200

    Add go generate cleanup step
    because the new k8s code generator adds unused go imports.

--- generate.go
+++ generate.go
@@ -1,5 +1,6 @@
 //go:generate go run ./cmd/codegen/cleanup/main.go
 //go:generate go run ./cmd/codegen/main.go
 //go:generate bash ./cmd/codegen/hack/generate_and_sort_crds.sh ./charts/fleet-crd/templates/crds.yaml
+//go:generate go run ./cmd/codegen/post-cleanup/main.go  // Clean up unused imports
 
 package main
