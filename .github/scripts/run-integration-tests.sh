b2f2ca0b4b701c94e4ef5f256c92bb5a3019b4be
Tim Hardeck <thardeck@suse.com>
2025-05-13 16:15:48 +0200

    Update ENVTEST_K8S_VERSION to 1.32 in integration test scripts

--- .github/scripts/run-integration-tests.sh
+++ .github/scripts/run-integration-tests.sh
@@ -3,7 +3,7 @@
 set -euxo pipefail
 
 SETUP_ENVTEST_VER=${SETUP_ENVTEST_VER-v0.0.0-20221214170741-69f093833822}
-ENVTEST_K8S_VERSION=${ENVTEST_K8S_VERSION-1.25}
+ENVTEST_K8S_VERSION=${ENVTEST_K8S_VERSION-1.32}
 
 # install and prepare setup-envtest
 go install sigs.k8s.io/controller-runtime/tools/setup-envtest@"$SETUP_ENVTEST_VER"
