69ff91afe12f7361bab63b6c359b6a98ca7da5bd
Corentin Néau <tan.neau@suse.com>
2025-05-14 13:35:46 +0200

    Monitor known_hosts entries through updatecli (#3680)
    * Add updatecli pipeline updating known-hosts config map
    
    This will allow that config map to be synchronised as often as updatecli
    pipelines run, hence daily, with updated `known_hosts` entries from
    listed providers.
    
    * Run script as external file
    
    This prevents syntax errors seen when inlining the script.
    
    * Replace `title` with `name`
    
    `title` as a field name is deprecated in favour of `name`, as stated by
    updatecli output.

---
+++ .github/scripts/update_known_hosts_configmap.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Write a new definition of the `known-hosts` config map in the fleet chart based on updated entries for each provider.
+# Entries are obtained through `ssh-keyscan` and sorted lexically to preserve ordering and hence prevent false
+# positives.
+providers=(
+    "bitbucket.org"
+    "github.com"
+    "gitlab.com"
+    "ssh.dev.azure.com"
+    "vs-ssh.visualstudio.com"
+)
+
+dst=charts/fleet/templates/configmap_known_hosts.yaml
+echo "apiVersion: v1" > "$dst"
+echo "kind: ConfigMap" >> "$dst"
+echo "metadata:" >> "$dst"
+echo "  name: known-hosts" >> "$dst"
+echo "data:" >> "$dst"
+echo "  known_hosts: |" >> "$dst"
+
+for prov in "${providers[@]}"; do
+    ssh-keyscan "$prov" | grep "^$prov" | sort -b | sed 's/^/    /' >> "$dst"
+done
