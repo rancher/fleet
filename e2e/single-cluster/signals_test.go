5725e7522a6ecc87ca3a227875698f4733a6765c
Corentin Néau <tan.neau@suse.com>
2025-07-29 12:02:52 +0200

    Add tests for signal handling
    New end-to-end test cases for SIGTERM handling should uncover possible
    future regressions such as the one fixed by the previous commit.

---
+++ e2e/single-cluster/signals_test.go
@@ -0,0 +1,126 @@
+package singlecluster_test
+
+import (
+	"encoding/json"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/rancher/fleet/e2e/testenv/kubectl"
+)
+
+type containerLastTerminatedState struct {
+	ExitCode int    `json:"exitCode"`
+	Reason   string `json:"reason"`
+}
+
+var _ = Describe("Shuts down gracefully", func() {
+	var (
+		k  kubectl.Command
+		ns string
+	)
+
+	When("the agent receives SIGTERM", func() {
+		BeforeEach(func() {
+			ns = "cattle-fleet-local-system"
+			k = env.Kubectl.Namespace(ns)
+		})
+
+		JustBeforeEach(func() {
+			out, err := k.Run("exec", "deploy/fleet-agent", "--", "kill", "1")
+			Expect(err).ToNot(HaveOccurred(), out)
+		})
+
+		It("exits gracefully", func() {
+			Eventually(func(g Gomega) {
+				out, err := k.Get("pod", "-l", "app=fleet-agent", "-o", "jsonpath={.items[0].status.containerStatuses[0].lastState.terminated}")
+				g.Expect(err).ToNot(HaveOccurred())
+
+				var state containerLastTerminatedState
+				err = json.Unmarshal([]byte(out), &state)
+				g.Expect(err).ToNot(HaveOccurred())
+
+				g.Expect(state.ExitCode).To(Equal(0))
+				g.Expect(state.Reason).To(Equal("Completed"))
+			}).Should(Succeed())
+		})
+	})
+
+	When("the fleet-controller deployment receives SIGTERM", func() {
+		BeforeEach(func() {
+			ns = "cattle-fleet-system"
+			k = env.Kubectl.Namespace(ns)
+		})
+
+		JustBeforeEach(func() {
+			out, err := k.Run("exec", "deploy/fleet-controller", "--", "kill", "1")
+			Expect(err).ToNot(HaveOccurred(), out)
+		})
+
+		It("exits gracefully", func() {
+			Eventually(func(g Gomega) {
+				out, err := k.Get("pod", "-l", "app=fleet-controller", "-o", "jsonpath={.items[0].status.containerStatuses[?(@.name=='fleet-controller')].lastState.terminated}")
+				g.Expect(err).ToNot(HaveOccurred())
+
+				var state containerLastTerminatedState
+				err = json.Unmarshal([]byte(out), &state)
+				g.Expect(err).ToNot(HaveOccurred())
+
+				g.Expect(state.ExitCode).To(Equal(0))
+				g.Expect(state.Reason).To(Equal("Completed"))
+			}).Should(Succeed())
+		})
+	})
+
+	When("the gitjob deployment receives SIGTERM", func() {
+		BeforeEach(func() {
+			ns = "cattle-fleet-system"
+			k = env.Kubectl.Namespace(ns)
+		})
+
+		JustBeforeEach(func() {
+			out, err := k.Run("exec", "deploy/gitjob", "--", "kill", "1")
+			Expect(err).ToNot(HaveOccurred(), out)
+		})
+
+		It("exits gracefully", func() {
+			Eventually(func(g Gomega) {
+				out, err := k.Get("pod", "-l", "app=gitjob", "-o", "jsonpath={.items[0].status.containerStatuses[0].lastState.terminated}")
+				g.Expect(err).ToNot(HaveOccurred())
+
+				var state containerLastTerminatedState
+				err = json.Unmarshal([]byte(out), &state)
+				g.Expect(err).ToNot(HaveOccurred())
+
+				g.Expect(state.ExitCode).To(Equal(0))
+				g.Expect(state.Reason).To(Equal("Completed"))
+			}).Should(Succeed())
+		})
+	})
+
+	When("the helmops deployment receives SIGTERM", func() {
+		BeforeEach(func() {
+			ns = "cattle-fleet-system"
+			k = env.Kubectl.Namespace(ns)
+		})
+
+		JustBeforeEach(func() {
+			out, err := k.Run("exec", "deploy/helmops", "--", "kill", "1")
+			Expect(err).ToNot(HaveOccurred(), out)
+		})
+
+		It("exits gracefully", func() {
+			Eventually(func(g Gomega) {
+				out, err := k.Get("pod", "-l", "app=helmops", "-o", "jsonpath={.items[0].status.containerStatuses[0].lastState.terminated}")
+				g.Expect(err).ToNot(HaveOccurred())
+
+				var state containerLastTerminatedState
+				err = json.Unmarshal([]byte(out), &state)
+				g.Expect(err).ToNot(HaveOccurred())
+
+				g.Expect(state.ExitCode).To(Equal(0))
+				g.Expect(state.Reason).To(Equal("Completed"))
+			}).Should(Succeed())
+		})
+	})
+})
