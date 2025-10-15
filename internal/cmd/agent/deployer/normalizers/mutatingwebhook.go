a81859e608587d6a2585f97ae042e0e2d5597469
Tim Hardeck <thardeck@suse.com>
2025-10-15 08:09:24 +0200

    Remove legacy code supporting Kubernetes before v1.32
    Fleet only supports admissionregistration.k8s.io/v1 as v1beta1 was removed
    in Kubernetes 1.22.

--- internal/cmd/agent/deployer/normalizers/mutatingwebhook.go
+++ internal/cmd/agent/deployer/normalizers/mutatingwebhook.go
@@ -6,7 +6,6 @@ import (
 	"github.com/rancher/fleet/internal/cmd/agent/deployer/objectset"
 
 	adregv1 "k8s.io/api/admissionregistration/v1"
-	adregv1beta1 "k8s.io/api/admissionregistration/v1beta1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
@@ -25,35 +24,7 @@ func (m *MutatingWebhookNormalizer) Normalize(un *unstructured.Unstructured) err
 		return nil
 	}
 
-	if gvk.Version == "v1beta1" {
-		return m.convertMutatingWebhookV1beta1(un)
-	}
-
 	return m.convertMutatingWebhookV1(un)
-}
-
-func (m *MutatingWebhookNormalizer) convertMutatingWebhookV1beta1(un *unstructured.Unstructured) error {
-	var webhook adregv1beta1.MutatingWebhookConfiguration
-	err := runtime.DefaultUnstructuredConverter.FromUnstructured(un.Object, &webhook)
-	if err != nil {
-		logrus.Error("Failed to convert unstructured to webhook")
-		return nil
-	}
-
-	for i, config := range webhook.Webhooks {
-		if webhook.UID == "" && string(config.ClientConfig.CABundle) == "\n" {
-			live := lookupLive(un.GroupVersionKind(), un.GetName(), un.GetNamespace(), m.Live)
-			liveWebhook, ok := live.(*unstructured.Unstructured)
-			if !ok {
-				continue
-			}
-			if err := setMutatingWebhookV1beta1CacertNil(liveWebhook, i); err != nil {
-				logrus.Errorf("Failed to normalize webhook cacert, err: %v", err)
-				return nil
-			}
-		}
-	}
-	return nil
 }
 
 func (m *MutatingWebhookNormalizer) convertMutatingWebhookV1(un *unstructured.Unstructured) error {
@@ -85,32 +56,6 @@ func setMutatingWebhookV1CacertNil(un *unstructured.Unstructured, index int) err
 	err := runtime.DefaultUnstructuredConverter.FromUnstructured(un.Object, &webhook)
 	if err != nil {
 		logrus.Errorf("Failed to convert unstructured to webhook, err: %v", err)
-		return err
-	}
-
-	if index >= len(webhook.Webhooks) {
-		return nil
-	}
-	webhook.Webhooks[index].ClientConfig.CABundle = nil
-	newObj, err := runtime.DefaultUnstructuredConverter.ToUnstructured(&webhook)
-	if err != nil {
-		logrus.Errorf("Failed to convert unstructured to webhook, err: %v", err)
-		return err
-	}
-	if webhook.Webhooks != nil {
-		if err = unstructured.SetNestedField(un.Object, newObj["webhooks"], "webhooks"); err != nil {
-			logrus.Errorf("MutatingWebhook normalization error: %v", err)
-			return err
-		}
-	}
-	return nil
-}
-
-func setMutatingWebhookV1beta1CacertNil(un *unstructured.Unstructured, index int) error {
-	var webhook adregv1beta1.MutatingWebhookConfiguration
-	err := runtime.DefaultUnstructuredConverter.FromUnstructured(un.Object, &webhook)
-	if err != nil {
-		logrus.Error("Failed to convert unstructured to webhook")
 		return err
 	}
 
--- internal/cmd/agent/deployer/normalizers/validatingwebhook.go
+++ internal/cmd/agent/deployer/normalizers/validatingwebhook.go
@@ -6,7 +6,6 @@ import (
 	"github.com/rancher/fleet/internal/cmd/agent/deployer/objectset"
 
 	adregv1 "k8s.io/api/admissionregistration/v1"
-	adregv1beta1 "k8s.io/api/admissionregistration/v1beta1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 )
@@ -24,35 +23,7 @@ func (v *ValidatingWebhookNormalizer) Normalize(un *unstructured.Unstructured) e
 		return nil
 	}
 
-	if gvk.Version == "v1beta1" {
-		return v.convertValidatingWebhookV1beta1(un)
-	}
-
 	return v.convertValidatingWebhookV1(un)
-}
-
-func (v *ValidatingWebhookNormalizer) convertValidatingWebhookV1beta1(un *unstructured.Unstructured) error {
-	var webhook adregv1beta1.ValidatingWebhookConfiguration
-	err := runtime.DefaultUnstructuredConverter.FromUnstructured(un.Object, &webhook)
-	if err != nil {
-		logrus.Error("Failed to convert unstructured to webhook")
-		return nil
-	}
-
-	for i, config := range webhook.Webhooks {
-		if webhook.UID == "" && string(config.ClientConfig.CABundle) == "\n" {
-			live := lookupLive(un.GroupVersionKind(), un.GetName(), un.GetNamespace(), v.Live)
-			liveWebhook, ok := live.(*unstructured.Unstructured)
-			if !ok {
-				continue
-			}
-			if err := setValidatingWebhookV1beta1CacertNil(liveWebhook, i); err != nil {
-				logrus.Errorf("Failed to normalize webhook cacert, err: %v", err)
-				return nil
-			}
-		}
-	}
-	return nil
 }
 
 func (v *ValidatingWebhookNormalizer) convertValidatingWebhookV1(un *unstructured.Unstructured) error {
@@ -84,32 +55,6 @@ func setValidatingWebhookV1CacertNil(un *unstructured.Unstructured, index int) e
 	err := runtime.DefaultUnstructuredConverter.FromUnstructured(un.Object, &webhook)
 	if err != nil {
 		logrus.Errorf("Failed to convert unstructured to webhook, err: %v", err)
-		return err
-	}
-
-	if index >= len(webhook.Webhooks) {
-		return nil
-	}
-	webhook.Webhooks[index].ClientConfig.CABundle = nil
-	newObj, err := runtime.DefaultUnstructuredConverter.ToUnstructured(&webhook)
-	if err != nil {
-		logrus.Errorf("Failed to convert unstructured to webhook, err: %v", err)
-		return err
-	}
-	if webhook.Webhooks != nil {
-		if err = unstructured.SetNestedField(un.Object, newObj["webhooks"], "webhooks"); err != nil {
-			logrus.Errorf("ValidatingWebhook normalization error: %v", err)
-			return err
-		}
-	}
-	return nil
-}
-
-func setValidatingWebhookV1beta1CacertNil(un *unstructured.Unstructured, index int) error {
-	var webhook adregv1beta1.ValidatingWebhookConfiguration
-	err := runtime.DefaultUnstructuredConverter.FromUnstructured(un.Object, &webhook)
-	if err != nil {
-		logrus.Error("Failed to convert unstructured to webhook")
 		return err
 	}
 
