09c9e861bd757b0261a2d3dc1d49e7b292d9c639
Alejandro Ruiz <alejandro.ruiz@suse.com>
2025-11-24 17:42:29 +0100

    Replace null byte separator with length-padding to avoid collisions

--- internal/bundlereader/auth.go
+++ internal/bundlereader/auth.go
@@ -3,6 +3,7 @@ package bundlereader
 import (
 	"context"
 	"crypto/sha256"
+	"encoding/binary"
 	"encoding/hex"
 	"fmt"
 	"strconv"
@@ -22,8 +23,6 @@ type Auth struct {
 	// remember to update Hash() when adding/modifying fields
 }
 
-var nullBytes = []byte("\x00")
-
 func toByte(v bool) byte {
 	if v {
 		return byte(1)
@@ -33,6 +32,15 @@ func toByte(v bool) byte {
 
 func (a Auth) Hash() string {
 	hash := sha256.New()
+
+	// Use data length as delimiter to avoid possible collisions
+	lenBuf := make([]byte, 8)
+	writeField := func(data []byte) {
+		binary.LittleEndian.PutUint64(lenBuf, uint64(len(data)))
+		hash.Write(lenBuf)
+		hash.Write(data)
+	}
+
 	for _, v := range [][]byte{
 		[]byte(a.Username),
 		[]byte(a.Password),
@@ -41,9 +49,9 @@ func (a Auth) Hash() string {
 		{toByte(a.InsecureSkipVerify)},
 		{toByte(a.BasicHTTP)},
 	} {
-		hash.Write(v)
-		hash.Write(nullBytes)
+		writeField(v)
 	}
+
 	return hex.EncodeToString(hash.Sum(nil))
 
 }
