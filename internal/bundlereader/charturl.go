22f34d5fc7744c9051dfff519710dbd0783220b6
Corentin Néau <tan.neau@suse.com>
2025-12-08 07:11:09 +0100

    Fix Helm URL resolution (#4400)
    Recent refactoring around chart URL computations introduced a bug with
    `repo` fields not containing a trailing slash, where a chunk of the path
    would be skipped.

--- internal/bundlereader/charturl.go
+++ internal/bundlereader/charturl.go
@@ -109,9 +109,9 @@ func getOCIRepoClient(repoURI string, a Auth) (*remote.Repository, error) {
 	return r, nil
 }
 
-// chartURL returns the URL to the helm chart from a helm repo server, by
+// ChartURL returns the URL to the helm chart from a helm repo server, by
 // inspecting the repo's index.yaml
-func chartURL(location fleet.HelmOptions, auth Auth, isHelmOps bool) (string, error) {
+func ChartURL(location fleet.HelmOptions, auth Auth, isHelmOps bool) (string, error) {
 	if uri, ok := isOCIChart(location, isHelmOps); ok {
 		return uri, nil
 	}
@@ -328,10 +328,5 @@ func toAbsoluteURLIfNeeded(baseURL, chartURL string) (string, error) {
 		return chartURL, err
 	}
 
-	u, err := url.Parse(baseURL)
-	if err != nil {
-		return "", err
-	}
-
-	return u.ResolveReference(chartU).String(), nil
+	return url.JoinPath(baseURL, chartURL)
 }
--- internal/bundlereader/resources.go
+++ internal/bundlereader/resources.go
@@ -216,7 +216,7 @@ func addRemoteCharts(directories []directory, base string, charts []*fleet.HelmO
 				auth = Auth{}
 			}
 
-			chartURL, err := chartURL(*chart, auth, false)
+			chartURL, err := ChartURL(*chart, auth, false)
 			if err != nil {
 				return nil, fmt.Errorf("failed to resolve URL of %s: %w", downloadChartError(*chart), err)
 			}
