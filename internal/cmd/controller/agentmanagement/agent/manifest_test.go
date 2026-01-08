ed0b8d566d0cd33914294476fbc160edf820634b
Christian Danscheid <webD97@users.noreply.github.com>
2026-01-08 15:36:55 +0100

    Fix: Set Recreate deployment strategy when hostNetwork is enabled (#4467)
    This prevents a race condition where the new agent pod might soft-lock if
    the ports it needs are still in use by the old replica.
    
    (cherry picked from commit 6bbe1ad94f3ead0cb14b03f06bd1a0521caf6e78)

--- internal/cmd/controller/agentmanagement/agent/manifest_test.go
+++ internal/cmd/controller/agentmanagement/agent/manifest_test.go
@@ -133,16 +133,18 @@ func TestManifestAgentHostNetwork(t *testing.T) {
 	}
 
 	for _, testCase := range []struct {
-		name            string
-		getOpts         func() agent.ManifestOptions
-		expectedNetwork bool
+		name             string
+		getOpts          func() agent.ManifestOptions
+		expectedNetwork  bool
+		expectedStrategy appsv1.DeploymentStrategyType
 	}{
 		{
 			name: "DefaultSetting",
 			getOpts: func() agent.ManifestOptions {
 				return baseOpts
 			},
-			expectedNetwork: false,
+			expectedNetwork:  false,
+			expectedStrategy: appsv1.DeploymentStrategyType(""),
 		},
 		{
 			name: "With hostNetwork",
@@ -151,7 +153,8 @@ func TestManifestAgentHostNetwork(t *testing.T) {
 				withHostNetwork.HostNetwork = true
 				return withHostNetwork
 			},
-			expectedNetwork: true,
+			expectedNetwork:  true,
+			expectedStrategy: appsv1.RecreateDeploymentStrategyType,
 		},
 	} {
 		t.Run(testCase.name, func(t *testing.T) {
@@ -162,6 +165,10 @@ func TestManifestAgentHostNetwork(t *testing.T) {
 
 			if !cmp.Equal(agent.Spec.Template.Spec.HostNetwork, testCase.expectedNetwork) {
 				t.Fatalf("hostNetwork is not as expected: %v", agent.Spec.Template.Spec.HostNetwork)
+			}
+
+			if !cmp.Equal(agent.Spec.Strategy.Type, testCase.expectedStrategy) {
+				t.Fatalf("strategy is not as expected: %v", agent.Spec.Strategy.Type)
 			}
 		})
 	}
