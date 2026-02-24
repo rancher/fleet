bcefff4cc6209135c2697dfa88b50e27299f21f0
Corentin Néau <tan.neau@suse.com>
2026-02-24 18:02:48 +0100

    Add unit tests for overwritten resources detection
    Templated resource names and namespaces are less likely than templated
    contents, but those cases are now covered through unit tests.

--- internal/cmd/cli/apply/apply_test.go
+++ internal/cmd/cli/apply/apply_test.go
@@ -7,11 +7,16 @@ import (
 )
 
 func Test_getKindNS(t *testing.T) {
-	// TODO add test cases covering templating in name and namespace (less likely in kind)
-	// → we should return an empty overwrite in such cases
-	br := fleet.BundleResource{
-		Name: "folder/my-cm.yaml",
-		Content: `apiVersion: v1
+	cases := []struct {
+		name           string
+		input          fleet.BundleResource
+		expectedOutput fleet.OverwrittenResource
+	}{
+		{
+			name: "templated contents",
+			input: fleet.BundleResource{
+				Name: "folder/my-cm.yaml",
+				Content: `apiVersion: v1
 kind: ConfigMap
 metadata:
   name: Foo
@@ -19,24 +24,66 @@ metadata:
 data:
   bar: baz
   name: {{ .Values.name }}`,
-		// No encoding
+				// No encoding
+			},
+			expectedOutput: fleet.OverwrittenResource{
+				Kind:      "ConfigMap",
+				Name:      "Foo",
+				Namespace: "my-namespace",
+			},
+		},
+		{
+			name: "templated name",
+			input: fleet.BundleResource{
+				Name: "folder/my-cm.yaml",
+				Content: `apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: {{ .Values.name }}
+  namespace: my-namespace
+data:
+  bar: baz
+  name: Foo`,
+				// No encoding
+			},
+			expectedOutput: fleet.OverwrittenResource{
+				Kind:      "ConfigMap",
+				Name:      "TEMPLATED",
+				Namespace: "my-namespace",
+			},
+		},
+		{
+			name: "templated namespace",
+			input: fleet.BundleResource{
+				Name: "folder/my-cm.yaml",
+				Content: `apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: Foo
+  namespace: {{ .Values.namespace }}
+data:
+  bar: baz
+  name: Foo`,
+				// No encoding
+			},
+			expectedOutput: fleet.OverwrittenResource{
+				Kind:      "ConfigMap",
+				Name:      "Foo",
+				Namespace: "TEMPLATED",
+			},
+		},
 	}
 
-	or, err := getKindNS(br, "my-bundle")
-	if err != nil {
-		t.Fatalf("expected no error, got %v", err)
-	}
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			or, err := getKindNS(tc.input, "my-bundle")
+			if err != nil {
+				t.Fatalf("expected no error, got %v", err)
+			}
 
-	if or.Kind != "ConfigMap" {
-		t.Fatalf("expected ConfigMap kind, got %v", or.Kind)
+			if or != tc.expectedOutput {
+				t.Fatalf("expected OverwrittenResource\n\t%v,got\n\t%v", tc.expectedOutput, or)
+			}
+		})
 	}
-
-	if or.Name != "Foo" {
-		t.Fatalf("expected name Foo, got %v", or.Name)
-	}
-
-	if or.Namespace != "my-namespace" {
-		t.Fatalf("expected namespace my-namespace, got %v", or.Namespace)
-	}
-
 }
