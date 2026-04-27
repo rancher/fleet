1d20cfb6063b1f93e0816932ac59a7b5902c2dae
Tim Hardeck <thardeck@suse.com>
2026-04-27 09:15:25 +0200

    [v0.12] Align Helm deployer client construction (#5048)
    * Use consistent getter in Helm configuration
    
    createCfg used the base getter for both RESTClientGetter and KubeClient
    independently of the getter produced in getCfg. Accept a getter
    parameter in createCfg so callers control which getter is used for all
    client construction.
    
    * Use Helm configuration getter for valuesFrom client
    
    Construct the kubernetes.Interface used to resolve valuesFrom
    references from the Helm configuration's RESTClientGetter.

--- internal/helmdeployer/install_test.go
+++ internal/helmdeployer/install_test.go
@@ -1,14 +1,19 @@
 package helmdeployer
 
 import (
+	"context"
 	"fmt"
 	"runtime"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
+	fleet "github.com/rancher/fleet/pkg/apis/fleet.cattle.io/v1alpha1"
 	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	kubernetesfake "k8s.io/client-go/kubernetes/fake"
 )
 
 func TestValuesFrom(t *testing.T) {
@@ -59,4 +64,114 @@ func TestValuesFrom(t *testing.T) {
 	totalValues = mergeValues(totalValues, secretValues)
 	totalValues = mergeValues(totalValues, configMapValues)
 	a.Equal(expected, totalValues)
+}
+
+func TestValuesFromUsesExplicitNamespace(t *testing.T) {
+	a := assert.New(t)
+	r := require.New(t)
+
+	defaultNS := "helm-default"
+	explicitNS := "explicit-ns"
+
+	cm := corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{Name: "cm-explicit", Namespace: explicitNS},
+		Data:       map[string]string{DefaultKey: "cmVal: explicit"},
+	}
+
+	kubeClient := kubernetesfake.NewSimpleClientset(&cm)
+	h := &Helm{template: false}
+
+	opts := fleet.BundleDeploymentOptions{
+		Helm: &fleet.HelmOptions{
+			ValuesFrom: []fleet.ValuesFrom{{ConfigMapKeyRef: &fleet.ConfigMapKeySelector{
+				LocalObjectReference: fleet.LocalObjectReference{Name: "cm-explicit"},
+				Namespace:            explicitNS,
+			}}},
+		},
+	}
+
+	vals, err := h.getValues(context.TODO(), opts, defaultNS, kubeClient)
+	r.NoError(err)
+	a.Equal("explicit", vals["cmVal"])
+}
+
+func TestValuesFromUsesDefaultNamespaceWhenNoneSpecified(t *testing.T) {
+	a := assert.New(t)
+	r := require.New(t)
+
+	defaultNS := "helm-default"
+
+	cm := corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{Name: "cm-default", Namespace: defaultNS},
+		Data:       map[string]string{DefaultKey: "cmVal: fromDefault"},
+	}
+	sec := corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{Name: "sec-default", Namespace: defaultNS},
+		Data:       map[string][]byte{DefaultKey: []byte("secVal: fromDefault")},
+	}
+
+	kubeClient := kubernetesfake.NewSimpleClientset(&cm, &sec)
+	h := &Helm{template: false}
+
+	opts := fleet.BundleDeploymentOptions{
+		Helm: &fleet.HelmOptions{
+			ValuesFrom: []fleet.ValuesFrom{
+				{ConfigMapKeyRef: &fleet.ConfigMapKeySelector{LocalObjectReference: fleet.LocalObjectReference{Name: "cm-default"}}},
+				{SecretKeyRef: &fleet.SecretKeySelector{LocalObjectReference: fleet.LocalObjectReference{Name: "sec-default"}}},
+			},
+		},
+	}
+
+	vals, err := h.getValues(context.TODO(), opts, defaultNS, kubeClient)
+	r.NoError(err)
+	a.Equal("fromDefault", vals["cmVal"])
+	a.Equal("fromDefault", vals["secVal"])
+}
+
+func TestValuesFromReturnsNotFoundWhenResourceMissing(t *testing.T) {
+	r := require.New(t)
+
+	kubeClient := kubernetesfake.NewSimpleClientset()
+	h := &Helm{template: false}
+
+	opts := fleet.BundleDeploymentOptions{
+		Helm: &fleet.HelmOptions{
+			ValuesFrom: []fleet.ValuesFrom{
+				{ConfigMapKeyRef: &fleet.ConfigMapKeySelector{
+					LocalObjectReference: fleet.LocalObjectReference{Name: "missing"},
+					Namespace:            "some-ns",
+				}},
+			},
+		},
+	}
+
+	_, err := h.getValues(context.TODO(), opts, "default-ns", kubeClient)
+	r.Error(err)
+	r.True(apierrors.IsNotFound(err), "expected a NotFound error when valuesFrom references a missing resource")
+}
+
+func TestValuesFromSkipsLookupInTemplateMode(t *testing.T) {
+	a := assert.New(t)
+	r := require.New(t)
+
+	// nil kubeClient signals template mode — no lookups should be performed
+	h := &Helm{template: true}
+
+	opts := fleet.BundleDeploymentOptions{
+		Helm: &fleet.HelmOptions{
+			Values: &fleet.GenericMap{Data: map[string]interface{}{"static": "value"}},
+			ValuesFrom: []fleet.ValuesFrom{
+				{ConfigMapKeyRef: &fleet.ConfigMapKeySelector{
+					LocalObjectReference: fleet.LocalObjectReference{Name: "should-not-be-read"},
+					Namespace:            "some-ns",
+				}},
+			},
+		},
+	}
+
+	vals, err := h.getValues(context.TODO(), opts, "default", nil)
+	r.NoError(err)
+	a.Equal("value", vals["static"])
+	_, hasUnwanted := vals["should-not-be-read"]
+	a.False(hasUnwanted, "template mode should not read from cluster")
 }
