5c5415ef6b6fbe69b5b683ffb149daae95a32670
Xavi Garcia <xavi.garcia@suse.com>
2026-08-10 15:03:11 +0200

    update warning message
    Signed-off-by: Xavi Garcia <xavi.garcia@suse.com>

--- internal/cmd/controller/agentmanagement/controllers/clusterregistration/controller.go
+++ internal/cmd/controller/agentmanagement/controllers/clusterregistration/controller.go
@@ -530,7 +530,7 @@ func sanitizeClusterLabels(request *fleet.ClusterRegistration) map[string]string
 	labels := make(map[string]string, len(request.Spec.ClusterLabels))
 	for k, v := range request.Spec.ClusterLabels {
 		if isReservedLabel(k) {
-			logrus.Warnf("ClusterRegistration %s/%s attempted to set reserved label %q; ignoring it",
+			logrus.Warnf("ClusterRegistration %s/%s attempted to set reserved label %q; ignoring that label",
 				request.Namespace, request.Name, k)
 			continue
 		}
